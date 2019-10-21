77ab8e9a5a359f9dee5be2039586a8a3851988fa
akash <akash.kumar@prolitus.com>
2019-10-21 17:01:17 +0530

    removed extra files and routes

--- resources/views/layouts/withought_login.blade.php
+++ resources/views/layouts/withought_login.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
     <head>
-        <title>Login V14</title>
+        <title>Zuron Login</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600&display=swap" rel="stylesheet">
