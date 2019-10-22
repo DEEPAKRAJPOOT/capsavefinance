2f172a7c0d4743322bbb636417ea2a5ffde5ada1
gajendra <gajendra.singh@prolitus.com>
2019-10-22 10:58:49 +0530

    new

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -63,7 +63,7 @@
                     </form>
 		</div>
 	</div>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
 <script>
 $(document).ready(function(){
 $("#passwordonoff").click(function(){
