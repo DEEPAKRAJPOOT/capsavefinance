94284c4bd014af1b638a58678f739cac599e0aa4
akash <akash.kumar@prolitus.com>
2019-11-12 14:28:08 +0530

    document upload

--- app/Inv/Repositories/Models/AppDocumentFile.php
+++ app/Inv/Repositories/Models/AppDocumentFile.php
@@ -77,24 +77,27 @@ class AppDocumentFile extends Authenticatable
         $inputArr = [];
         $count = count($attributes['bank_docs']);
         for ( $i=0; $i < $count; $i++) 
-        {
+        {   
+//            dd($attributes['bank_docs'][$i]->getClientOriginalExtension());
             if($attributes['bank_docs'][$i]) {
                 if(!Storage::exists('/public/uploads/1/bank_document')) {
                     Storage::makeDirectory('/public/uploads/1/bank_document', 0775, true);
                 }
-                $path = Storage::disk('public')->put('/uploads/1/bank_document', $attributes['bank_docs'][$i],null);
+                
+                $path = Storage::disk('public')->put('/uploads/1/bank_document', $attributes['bank_docs'][$i], null);
                 $inputArr[$i]['file_path'] = $path;
             }
+            
             $inputArr[$i]['app_id']  = 1;   
             $inputArr[$i]['doc_id']  = $mstDocId; 
             $inputArr[$i]['is_upload'] = $attributes['bank_docs'][$i];
             $inputArr[$i]['file_type'] = $attributes['bank_docs'][$i]->getClientMimeType();
             $inputArr[$i]['file_name'] = $attributes['bank_docs'][$i]->getClientOriginalName();
             $inputArr[$i]['file_size'] = $attributes['bank_docs'][$i]->getClientSize();
-            $inputArr[$i]['file_encp_key'] = 1;
+            $inputArr[$i]['file_encp_key'] =  md5('2');
             $inputArr[$i]['created_by'] = 1;
         }
-        dd($inputArr);
+        
         return $inputArr;
   }
   
