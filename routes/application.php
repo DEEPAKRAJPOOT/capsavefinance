99d14e8252bb58b17e09f7eef5be8675dbbcb5e3
pankaj.sharma <pankaj.sharma@prolitus.com>
2019-11-16 14:47:30 +0530

    999

--- routes/application.php
+++ routes/application.php
@@ -86,33 +86,5 @@ Route::domain(config('proin.frontend_uri'))->group(function () {
             ]);
         });
     });
-
-    Route::group(['prefix' => 'profile'],
-        function () {
-        Route::group(['middleware' => 'auth'],
-            function () {
-
-            Route::get('/',
-                [
-                'as' => 'profile',
-                'uses' => 'Application\AccountController@index'
-            ]);
-           /* 
-            Route::get('edit',
-                [
-                'as' => 'edit_profile',
-                'uses' => 'Application\AccountController@editPersonalProfile'
-            ]);*/
-            
-            Route::post('edit',
-                [
-                'as' => 'update_personal_profile',
-                'uses' => 'Application\AccountController@savePersonalProfile'
-            ]);
-            
-
-        });
-    });
-    
-//   Resource controller @Supplier,@buyer,@lender,@logistics    //    
+     
 });    
