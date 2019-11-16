e3a74b20437b5eeb62fbeee1b36dbf592c02c026
Anuj Chauhan <anuj.chauhan@prolitus.com>
2019-11-16 14:11:04 +0530

    1212

--- resources/views/frontend/application/business-information.blade.php
+++ resources/views/frontend/application/business-information.blade.php
@@ -537,7 +537,7 @@
 
 	function checkValidation(){
 		if($('.pan-verify').text() == 'Verify'){
-			alert('First verify Business PAN');
+			alert('Please fill and verify Business PAN First');
 			return false
 		}else{
 			return true;
