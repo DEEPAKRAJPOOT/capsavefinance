88aaa8250188fca7015144c717bdea6097ac8a1f
Ravi Prakash <ravi.awasthi@prolitus.com>
2019-11-18 16:10:05 +0530

    subnav added

--- resources/views/backend/cam/overview.blade.php
+++ resources/views/backend/cam/overview.blade.php
@@ -2,27 +2,6 @@
 
 @section('content')
 
-<ul class="main-menu">
-    <li>
-        <a href="company-details.php" class="active">Application details</a>
-    </li>
-    <li>
-        <a href="cam.php">CAM</a>
-    </li>
-    <li>
-        <a href="residence.php">FI/RCU</a>
-    </li>
-    <li>
-        <a href="Collateral.php">Collateral</a>
-    </li>
-    <li>
-        <a href="notes.php">Notes</a>
-    </li>
-    <li>
-        <a href="commercial.php">Submit Commercial</a>
-    </li>
-</ul>
-
 <div class="content-wrapper">
     <ul class="sub-menu-main pl-0 m-0">
 
