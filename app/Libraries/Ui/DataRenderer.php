d4eec917f4b797c687304e51b79f8b14ef5ef158
pankaj.sharma <pankaj.sharma@prolitus.com>
2019-11-18 15:55:58 +0530

    999

--- app/Libraries/Ui/DataRenderer.php
+++ app/Libraries/Ui/DataRenderer.php
@@ -184,7 +184,7 @@ class DataRenderer implements DataProviderInterface
                     'action',
                     function ($app) {
                         //return  "<a  data-toggle=\"modal\" data-target=\"#editLead\" data-url =\"" . route('edit_backend_lead', ['user_id' => $users->user_id]) . "\" data-height=\"500px\" data-width=\"100%\" data-placement=\"top\" class=\"btn btn-warning btn-sm  report-btn btn-x-sm\"><i class=\"fa fa-edit\"></a>";
-                    return '<div class="d-flex inline-action-btn"><a title="Add App Note" href="#" data-toggle="modal" data-target="#myModal" class="btn btn-action-btn btn-sm"><i class="fa fa-file-image-o" aria-hidden="true"></i></a>
+                    return '<div class="d-flex inline-action-btn"><a title="Add App Note" href="#" data-toggle="modal" data-target="#noteFrame" data-url ="' . route('backend_notes_from') . '" data-height="500px" data-width="100%" data-placement="top" class="btn btn-action-btn btn-sm"><i class="fa fa-file-image-o" aria-hidden="true"></i></a>
 									   <a href="#" data-toggle="modal" data-target="#myModal1" title="Add Status" class="btn btn-action-btn btn-sm"><i class="fa fa-outdent" aria-hidden="true"></i></a>
 									   <a href="#" title="Assign Case" data-toggle="modal" data-target="#myModal2" class="btn btn-action-btn btn-sm"><i class="fa fa-window-restore" aria-hidden="true"></i></a> </div>';
                     }
