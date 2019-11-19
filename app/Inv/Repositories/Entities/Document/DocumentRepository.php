3ab014e0e43aebeada0a50f0a6c5c921d0cb5841
Anuj Chauhan <anuj.chauhan@prolitus.com>
2019-11-19 18:24:49 +0530

    4323

--- app/Inv/Repositories/Entities/Document/DocumentRepository.php
+++ app/Inv/Repositories/Entities/Document/DocumentRepository.php
@@ -81,13 +81,14 @@ class DocumentRepository implements DocumentInterface
      */
     
     public function findRequiredDocs($userId, $appId){
-        $appData = BizOwner::getAppId($userId);
-        $appId = (!empty($appData)) ? $appData->app_id : 0;
+        // $appData = BizOwner::getAppId($userId);
+        $appId = (!empty($appId)) ? $appId : 0;
+        // dd($appId);
         $result = AppDocument::where('user_id', $userId)
                 ->where('app_id', $appId)
                 ->with('document')
                 ->get();
-        
+        // dd($result);
         return $result ?: false;
     }
     
