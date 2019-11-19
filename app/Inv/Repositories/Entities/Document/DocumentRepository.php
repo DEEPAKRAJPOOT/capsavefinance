83548ea6c0c1c61b943739b6c25392ef15f6afd0
akash <akash.kumar@prolitus.com>
2019-11-19 19:27:50 +0530

    cjange

--- app/Inv/Repositories/Entities/Document/DocumentRepository.php
+++ app/Inv/Repositories/Entities/Document/DocumentRepository.php
@@ -82,7 +82,7 @@ class DocumentRepository implements DocumentInterface
     
     public function findRequiredDocs($userId, $appId){
          $appData = BizOwner::getAppId($userId);
-        $appId = (!empty($appId)) ? $appId : 0;
+        $appId = (!empty($appData->app_id)) ? $appData->app_id : 0;
         // dd($appId);
         $result = AppDocument::where('user_id', $userId)
                 ->where('app_id', $appId)
