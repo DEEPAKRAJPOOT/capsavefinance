6ab6f6a1bd8e82875ff258cd7329f1978ca75ca2
gajendra <gajendra.singh@prolitus.com>
2019-12-17 15:53:43 +0530

    changing

--- resources/views/backend/app/promoter-details.blade.php
+++ resources/views/backend/app/promoter-details.blade.php
@@ -659,6 +659,7 @@
         if ($('form#signupForm').validate().form()) {
         var panCount = 0;
         var promoCount = 0;
+        var total = 0;
         var DlLength = $('input[name="dlfile[]"]').length;
         var total = 0;
         ///// for upload one in three id proff..............
@@ -741,6 +742,22 @@
                 return true;
         }
         });
+
+        ////// Combination of Shareholding (%) should  not exceed more than 100 %///////////
+        $(".share_per").each(function (k, v) { 
+            if($(this).val()!='')
+            {
+                total += parseInt($(this).val());
+            }
+        });
+       
+        if(total > 100)
+        {
+          
+           alert('Combination of Shareholding (%) should  not exceed more than 100 %');
+            e.preventDefault();
+           return false;
+       }
         var form = $("#signupForm");
         $('.isloader').show();
         $.ajax({
@@ -925,7 +942,6 @@
         {
                 $(".owneridDynamic").each(function(k, v){
                 var GetVal = $(this).val();
-                alert(v);
                 if (GetVal == '')
                 {
                     $("#submit").val('Save');
@@ -1071,6 +1087,7 @@
                 var status = data['status-code'];
                 if (data['status-code'] == 101)
                 {
+                $("#veripan"+count).val(PAN);   
                 var MergeResonse = name_status.concat(request_id, status);
                 $('#response' + count).val(MergeResonse);
                 $('#pan_no' + count).attr('readonly', true);
@@ -1121,6 +1138,7 @@
                 success: function (data) {
                 if (data['status'] == 1)
                 {
+                
                 $('#veripan' + count).attr('readonly', true);
                 $('#ppan' + count).text('Verified');
                 $('#ppan' + count).css('pointer-events', 'none');
