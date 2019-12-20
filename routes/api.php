1d58081f24a6c0722a5800229d246eb0759013e5
Ravi Prakash <ravi.awasthi@prolitus.com>
2019-12-20 13:36:41 +0530

    0987654321

--- routes/api.php
+++ routes/api.php
@@ -16,22 +16,3 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-
- Route::any('api/perfios/fsa-callback',[
-        'as' => 'api_perfios_fsa_callback',
-        'uses' => 'Auth\ApiController@fsa_callback'
-        ]
-    );
-
-    Route::any('api/perfios/bsa-callback',[
-        'as' => 'api_perfios_bsa_callback',
-        'uses' => 'Auth\ApiController@bsa_callback'
-        ]
-    );
-
-    Route::any('api/karza/webhook',[
-        'as' => 'api_karza_webhook',
-        'uses' => 'Auth\ApiController@karza_webhook'
-        ]
-    );
