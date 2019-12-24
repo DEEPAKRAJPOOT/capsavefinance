e7a4c76e0f323a6a05ded01fa78259a42bfafc63
akash <akash.kumar@prolitus.com>
2019-12-24 17:57:46 +0530

    changes

--- app/Http/Controllers/Backend/DocumentController.php
+++ app/Http/Controllers/Backend/DocumentController.php
@@ -7,6 +7,7 @@ use App\Inv\Repositories\Contracts\ApplicationInterface as InvAppRepoInterface;
 use App\Inv\Repositories\Contracts\DocumentInterface as InvDocRepoInterface;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\DocumentRequest;
 use App\Inv\Repositories\Models\User;
 use Session;
 use Helpers;
@@ -68,93 +69,89 @@ class DocumentController extends Controller
     /**
      * Show assign RCU 
      */
-    public function showAssignRcu(Request $request)
+    public function uploadDocument(Request $request)
     {
-        $agencies = $this->appRepo->getAllAgency();
-        $agency_users = $this->userRepo->getAllAgencyUsers();
-        
-        return view('backend.fircu.rcu_trigger')->with(['agencies'=>$agencies, 'agency_users'=>$agency_users]);   
+        return view('backend.document.upload_document');   
     }
-
-    /**
-     * Save assign RCU 
+/**
+     * Handle a Business documents for the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
-    public function saveAssignRcu(Request $request)
+    
+    public function saveDocument(DocumentRequest $request)
     {
-        $appData = $this->appRepo->getAppDataByAppId($request->get('app_id'));
-        $requestAll = $request->all();
-        $appId = $request->get('app_id');
-        $documentIds = $request->get('document_ids');
-        
-        $docIds = explode('#', trim($documentIds, '#'));
-        $rcuDocArr = [];
-        $rcuStatusLogArr = [];
-        if($requestAll) {
-            
-            $rcuDocArr['app_id']=$requestAll['app_id'];
-            $rcuDocArr['agency_id']=$requestAll['agency_id'];
-            $rcuDocArr['from_id']=Auth::user()->user_id;
-            $rcuDocArr['to_id']=$requestAll['to_id'];
-            $rcuDocArr['rcu_status_id']= 2;
-            $rcuDocArr['rcu_status_updated_by']=0;
-            $rcuDocArr['rcu_comment']=$requestAll['comment'];
-            $rcuDocArr['cm_rcu_status_id']=1;
-            $rcuDocArr['file_id']= 0;
-            $rcuDocArr['is_active']= 1;
-            $rcuDocArr['created_at']=\Carbon\Carbon::now();
-            $rcuDocArr['created_by']=Auth::user()->user_id;
-            $rcuStatusLogArr['rcu_status_id']=2;
-            $rcuStatusLogArr['rcu_comment']=$requestAll['comment'];
-            $rcuStatusLogArr['created_at']=\Carbon\Carbon::now();
-            $rcuStatusLogArr['created_by']=Auth::user()->user_id;
-        }
-        
-        foreach ($docIds as $key=>$value) {
-            $rcuDocArr['doc_id']=$value;
-            $rcuDocResponse = $this->appRepo->assignRcuDocument($rcuDocArr);
-            
-            if($rcuDocResponse == 'Assigned') {
-                Session::flash('message',trans('success_messages.rcu.alreadyAssigned'));
-                return redirect()->route('backend_rcu', ['app_id' => request()->get('app_id'), 'biz_id' => $appData->biz_id]);
+        try {
+            // dd($request->all());
+            $arrFileData = $request->all();
+            $docId = (int)$request->doc_id; //  fetch document id
+            $appId = (int)$request->app_id; //  fetch app id
+            $bizId = (int)$request->biz_id; //  fetch biz id
+            $userData = $this->userRepo->getUserByAppId($appId);
+            $userId = $userData->user_id;
+
+            switch ($docId) {
+                case '4':
+                    $file_bank_id = $arrFileData['file_bank_id'];
+                    $bankData = State::getBankName($file_bank_id);
+                    $arrFileData['doc_name'] = $bankData['bank_name'] ?? NULL;
+                    $arrFileData['finc_year'] = NULL;
+                    $arrFileData['gst_month'] = $arrFileData['bank_month'];
+                    $arrFileData['gst_year'] = $arrFileData['bank_year'];
+                    $arrFileData['pwd_txt'] = $arrFileData['is_pwd_protected'] ? $arrFileData['pwd_txt'] :NULL;
+                    break;
+                case '5':
+                    $arrFileData['file_bank_id'] = NULL;
+                    $arrFileData['gst_month'] = NULL;
+                    $arrFileData['gst_year'] = NULL;
+                    $arrFileData['pwd_txt'] = $arrFileData['is_pwd_protected'] ? $arrFileData['pwd_txt'] :NULL;
+                    break;
+
+                case '6':
+                    $arrFileData['file_bank_id'] = NULL;
+                    $arrFileData['finc_year']    = NULL;
+                    $arrFileData['is_pwd_protected'] = NULL;
+                    $arrFileData['is_scanned'] = NULL;
+                    $arrFileData['pwd_txt'] = NULL;
+                    break;
                 
+                default:
+                    //$arrFileData = "Invalid Doc ID";
+                    $arrFileData['file_bank_id'] = NULL;
+                    $arrFileData['finc_year']    = NULL;
+                    $arrFileData['is_pwd_protected'] = NULL;
+                    $arrFileData['is_scanned'] = NULL;
+                    $arrFileData['pwd_txt'] = NULL;                                        
+                    break;
             }
-            if($rcuDocResponse) {
-                $rcuStatusLogArr['rcu_doc_id'] = $rcuDocResponse->rcu_doc_id;
-                $result = $this->appRepo->saveRcuStatusLog($rcuStatusLogArr);
-            }
+            // dd($arrFileData);
+            $document_info = $this->docRepo->saveDocument($arrFileData, $docId, $userId);
+            if ($document_info) {
+                //Add/Update application workflow stages    
+                $response = $this->docRepo->isUploadedCheck($userId, $appId);            
+                $wf_status = $response->count() < 1 ? 1 : 2;
                 
+                $currentStage = Helpers::getCurrentWfStage($appId);            
+                $curr_wf_stage_code = $currentStage ? $currentStage->stage_code : null;
+                if ($curr_wf_stage_code == 'doc_upload') {
+                    Helpers::updateWfStage('doc_upload', $appId, $wf_status);
+                } else if($curr_wf_stage_code == 'upload_exe_doc') {
+                    Helpers::updateWfStage('upload_exe_doc', $appId, $wf_status);
+                }
+                Session::flash('message',trans('success_messages.uploaded'));
+                return redirect()->route('pp_document_list', ['app_id' => $appId, 'biz_id' => $bizId]);
+            } else {
+                //Add application workflow stages
+                Helpers::updateWfStage('doc_upload', $appId, $wf_status=2);
+                return redirect()->route('pp_document_list', ['app_id' => $appId, 'biz_id' => $bizId]);
+            }
+        } catch (Exception $ex) {
+            //Add application workflow stages
+            Helpers::updateWfStage('doc_upload', $request->get('appId'), $wf_status=2);
+                
+            return redirect()->route('pp_document_list', ['app_id' => $appId, 'biz_id' => $bizId])->withErrors(Helpers::getExceptionMessage($ex));
         }
-        
-        Session::flash('message',trans('success_messages.rcu.assigned'));
-        return redirect()->route('backend_rcu', ['app_id' => request()->get('app_id'), 'biz_id' => $appData->biz_id]);   
-    }
-
-    /**
-     * Display Rcu upload modal
-     */
-    public function RcuUpload(Request $request)
-    {
-        return view('backend.fircu.rcu_upload_file');   
-    }
-
-    /**
-     * Save FI upload File
-     */
-    public function saveRcuUpload(Request $request)
-    {
-        $app_id = $request->get('app_id');
-        $biz_id = $request->get('biz_id');
-        $rcuDocId = $request->rcu_doc_id;
-        $uploadData = Helpers::uploadAppFile($request->all(), $app_id);
-        $userFile = $this->docRepo->saveFile($uploadData);
-
-        $status = $this->appRepo->updateRcuFile($userFile, $rcuDocId);
-        if($status){
-            Session::flash('message',trans('success_messages.uploaded'));
-        }else{
-            Session::flash('message',trans('auth.oops_something_went_wrong'));
-        }
-        return redirect()->route('backend_rcu', ['app_id' => $app_id, 'biz_id' => $biz_id]); 
     }
 
 }
--- public/common/js/iframePopup.js
+++ public/common/js/iframePopup.js
@@ -538,5 +538,19 @@ $(document).ready(function () {
                         }
                 );
         });
+
+        $("#ppUploadDocument").on('show.bs.modal', function (e) {
+                var parent = $(e.relatedTarget);
+                var height = parent.attr('data-height');
+                var url = parent.attr('data-url');
+                var width = parent.attr('data-width');
+                $("#ppUploadDocument iframe").attr(
+                        {
+                            'src': url,
+                            'height': height,
+                            'width': width
+                        }
+                );
+        });
       
 });
