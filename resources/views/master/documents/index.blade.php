d04bcf6a2e4d7117ab942853a5af6223ef3a9b53
akash <akash.kumar@prolitus.com>
2020-01-21 18:59:09 +0530

    master document changes

--- resources/views/master/documents/index.blade.php
+++ resources/views/master/documents/index.blade.php
@@ -27,7 +27,7 @@
                     <button type="button" id="searchbtn" class="btn btn-success btn-sm float-right">Search</button>
                 </div>
                 <div class="col-md-7 text-right">
-                    <a data-toggle="modal" class="btn  btn-success btn-sm" data-target="#addDocumentsFrame" data-url ="{{route('add_documents')}}" data-height="400px" data-width="100%" data-placement="top" >
+                    <a data-toggle="modal" class="btn  btn-success btn-sm" data-target="#addDocumentsFrame" data-url ="{{route('add_documents')}}" data-height="320px" data-width="100%" data-placement="top" >
                             <i class="fa fa-plus"></i>Add Documents
                     </a>
                 </div>
