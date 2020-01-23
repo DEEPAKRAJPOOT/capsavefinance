655543cb3874ae893f4887f74759a8c191adcae3
brajesh.kumar <brajesh.kumar@prolitus.com>
2020-01-23 17:40:44 +0530

    show gst

--- resources/views/backend/cam/gstin.blade.php
+++ resources/views/backend/cam/gstin.blade.php
@@ -37,29 +37,168 @@
                   <div class="clearfix"></div>
                   <br/>
                   <hr>
+
+
+              @if($gstResponsShow)
+
+                   @php
+               $current_year = $gstResponsShow['current']['financial_year'];
+               $previous_year = $gstResponsShow['previous']['financial_year'];
+                   @endphp
+                  @if($gstResponsShow && $gstResponsShow['current']['turnover_and_customers'])
+                 <h3 class="text-center">Business Overview</h3>
+                 <span class="subBnShow">Turnover & Customers</span>
+               <table id="" class="table  GST-detail overview-table" cellspacing="0" width="100%">
+                 
+                  <tbody>
+                  <tr>
+                        <th>Financial Year  </th>
+                        <th> Gross Turnover </th>
+                        <th>Net Turnover  </th>
+                        <th>Total Customers  </th>
+                        <th>Total Invoices </th>
+                       </tr>  
+                       <tr>
+                        <td> {{$previous_year}}</td>
+                        <td>{!! \Helpers::formatCurreny($gstResponsShow['previous']['turnover_and_customers']['gross_turnover'])!!}   </td>
+                        <td>{!! \Helpers::formatCurreny($gstResponsShow['previous']['turnover_and_customers']['net_turnover'])!!}  </td>
+                        <td> {{$gstResponsShow['previous']['turnover_and_customers']['ttl_customer']}}</td> 
+                         <td> {{ $gstResponsShow['previous']['turnover_and_customers']['ttl_inv']}} </td>                       
+                     </tr>              
+                      <tr>
+                        <td> {{$current_year}}</td>
+                        <td>{!! \Helpers::formatCurreny($gstResponsShow['current']['turnover_and_customers']['gross_turnover'])!!}   </td>
+                        <td>{!! \Helpers::formatCurreny($gstResponsShow['current']['turnover_and_customers']['net_turnover'])!!}  </td>
+                        <td> {{$gstResponsShow['current']['turnover_and_customers']['ttl_customer']}}</td> 
+                         <td> {{ $gstResponsShow['current']['turnover_and_customers']['ttl_inv']}} </td>                       
+                     </tr>                   
+                  </tbody>
+                  <tbody>
+                  </tbody>
+               </table>               
+               @endif
+
+
+
+
+
+                 @if($currenttop3Cus) <!---start code for display top 3 customer-->
+                 <h3 class="text-center">Top 3 Customers</h3>
                <table id="" class="table  GST-detail overview-table" cellspacing="0" width="100%">
                   <thead>
-                     <tr>
-                        <th width="30%">Month/Year</th>
-                        <th>No Approved Buyer</th>
+                     <tr align="center">
+                        <th colspan="5" >{{$current_year}}</th>
                      </tr>
                   </thead>
                   <tbody>
-
-                     @for($i = 1; $i <= 12; $i++)
+                  <tr>
+                        <th>PAN  </th>
+                        <th> Name </th>
+                        <th>Turnover  </th>
+                        <th>Total Invoices  </th>
+                        <th>Share </th>
+                       </tr>
+                  @foreach($currenttop3Cus as $custVal)                   
                       <tr>
-                        <td>
-                           <div id="head2">
-                              {{ date("F,Y", strtotime("-$i month", strtotime(date('Y-m-d'))))}}
-                           </div>
-                        </td>
-                        <td colspan="2" style="padding-left: 0rem !important;padding-right: 0rem !important;padding:0;"></td>
+                        <td> {{ $custVal['pan']}}</td>
+                        <td>{{ $custVal['name']}}</td>
+                        <td> {!! \Helpers::formatCurreny($custVal['ttl_tax'])!!} </td>
+                        <td>{{ $custVal['ttl_rec']}}</td> 
+                         <td>{{ number_format($custVal['share']*100,2)}}%</td>                       
                      </tr>
-                     @endfor
+                     @endforeach
                   </tbody>
                   <tbody>
                   </tbody>
                </table>
+               <table id="" class="table  GST-detail overview-table" cellspacing="0" width="100%">
+                  <thead>
+                     <tr align="center">
+                        <th colspan="5" >{{$previous_year}}</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                        <th>PAN  </th>
+                        <th> Name </th>
+                        <th>Turnover  </th>
+                        <th>Total Invoices  </th>
+                        <th>Share </th>
+                       </tr>
+                  @foreach($previoustop3Cus as $custVal)                   
+                      <tr>
+                        <td>{{ $custVal['pan']}}</td>
+                        <td>{{ $custVal['name']}}  </td>
+                        <td>{!!\Helpers::formatCurreny($custVal['ttl_tax'])!!}</td>
+                        <td>{{ $custVal['ttl_rec']}}  </td> 
+                         <td> {{ number_format($custVal['share']*100,2)}}%</td>                       
+                     </tr>
+                     @endforeach
+                  </tbody>
+                  <tbody>
+                  </tbody>
+               </table>
+               @endif <!---end code for display top 3 customer-->
+
+             <!---start code for display top 3 supplier-->
+               @if($currenttop3Sup)
+                 <h3 class="text-center">Top 3 Suppliers</h3>
+               <table id="" class="table  GST-detail overview-table" cellspacing="0" width="100%">
+                  <thead>
+                     <tr align="center">
+                        <th colspan="5" >{{$current_year}}</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                        <th>PAN  </th>
+                        <th> Name </th>
+                        <th>Turnover  </th>
+                        <th>Total Invoices  </th>
+                        <th>Share </th>
+                       </tr>
+                  @foreach($currenttop3Sup as $custVal)                   
+                      <tr>
+                        <td> {{ $custVal['pan']}} </td>
+                        <td>{{ $custVal['name']}} </td>
+                        <td>{!! \Helpers::formatCurreny($custVal['ttl_tax'])!!} </td>
+                        <td> {{ $custVal['ttl_rec']}} </td> 
+                         <td>{{ number_format($custVal['share']*100,2)}}%</td>                       
+                     </tr>
+                     @endforeach
+                  </tbody>
+                  <tbody>
+                  </tbody>
+               </table>
+               <table id="" class="table  GST-detail overview-table" cellspacing="0" width="100%">
+                  <thead>
+                     <tr align="center">
+                        <th colspan="5" >{{$previous_year}}</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                        <th>PAN  </th>
+                        <th> Name </th>
+                        <th>Turnover  </th>
+                        <th>Total Invoices  </th>
+                        <th>Share </th>
+                       </tr>
+                  @foreach($previoustop3Sup as $custVal)                   
+                      <tr>
+                        <td>  {{ $custVal['pan']}}  </td>
+                        <td>{{ $custVal['name']}} </td>
+                        <td>{!!\Helpers::formatCurreny($custVal['ttl_tax'])!!}</td>
+                        <td>{{ $custVal['ttl_rec']}}</td> 
+                         <td>{{ number_format($custVal['share']*100,2)}}% </td>                       
+                     </tr>
+                     @endforeach
+                  </tbody>
+                  <tbody>
+                  </tbody>
+               </table>
+               @endif <!---end code for display top 3 supplier-->
+        @endif    <!--end gst  display code-->
             </div>
          </div>
          @if(request()->get('view_only')) 
