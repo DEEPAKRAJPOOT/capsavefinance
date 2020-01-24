8e88865c3b6978ffd1596be6dffc477ed929e995
Anuj Chauhan <anuj.chauhan@prolitus.com>
2020-01-24 10:25:27 +0530

    working on offer listview

--- resources/views/layouts/backend/partials/admin-subnav.blade.php
+++ resources/views/layouts/backend/partials/admin-subnav.blade.php
@@ -50,10 +50,10 @@
         $isNavAccessible = $currentStage->role_id == $roleData[0]->id ? 1 : 0;            
     @endphp
     
-    @if ($currentStage->stage_code == 'sales_queue' && $isNavAccessible)    
+    @if (1 ||$currentStage->stage_code == 'sales_queue' && $isNavAccessible)    
     @can('view_offer')    
     <li>
-        <a href="{{ route('view_offer', ['app_id' => request()->get('app_id'), 'biz_id' => request()->get('biz_id')]) }}" class="{{ request()->is('view-offer') ? 'active' : '' }}">View Offer</a>
+        <a href="{{ route('view_offer', ['app_id' => request()->get('app_id'), 'biz_id' => request()->get('biz_id')]) }}" class="{{ request()->is('application/view-offer') ? 'active' : '' }}">View Offer</a>
     </li>
     @endcan
     @endif
