3ddab951b5bd986e35aa3008e984504cae1c50d6
kuldeep.singh <kuldeep.singh@prolitus.com>
2020-01-28 15:16:04 +0530

    mail design updation

--- resources/views/backend/cam/reviewer_summary.blade.php
+++ resources/views/backend/cam/reviewer_summary.blade.php
@@ -4,13 +4,10 @@
 @include('layouts.backend.partials.admin-subnav')
 <div class="content-wrapper">
    @include('layouts.backend.partials.cam_nav')
-   <div class="inner-container">
+   <div class="inner-container mt-4">
       <div class="row">
          <div class="col-md-12">
-            <a href="{{route('mail_reviewer_summary', ['app_id' => request()->get('app_id'), 'biz_id' => request()->get('biz_id')])}}"><button type="submit" class="btn btn-success btn-sm float-right">
-            <h5><small>Send Mail</small></h5> 
-            </button>   
-            </a>                 
+            <a href="{{route('mail_reviewer_summary', ['app_id' => request()->get('app_id'), 'biz_id' => request()->get('biz_id')])}}"><button type="" class="btn btn-success btn-sm float-right">Send Mail</button></a>                 
          </div>
       </div>
       <!--Start-->
