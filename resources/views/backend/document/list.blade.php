0ba9e360e8e5d7bdd69a505199cdb03785b30578
akash <akash.kumar@prolitus.com>
2020-01-30 11:57:14 +0530

    document prepost change

--- resources/views/backend/document/list.blade.php
+++ resources/views/backend/document/list.blade.php
@@ -32,7 +32,7 @@
                  <div class="card card-color mb-0">
                     <div class="card-header">
                         <a class="card-title ">
-                            No document found.
+                            No pre/post stage document found.
                         </a>
 
                     </div>
@@ -51,10 +51,11 @@
                 <div id="accordion" class="accordion d-table col-sm-12">
                 @if($product['documents']->count() > 0)
                     @foreach($product['documents'] as $key=>$data)
+
                     <div class="card card-color mb-0">
                         <div class="card-header" data-toggle="collapse" href="#collapse{{ $data->app_doc_id }}">
                             <a class="card-title ">
-                                <b>{{ $data->ppDocument->doc_name }}</b>
+                                <b>{{ $data->ppDocument->doc_name }} ( {{ ($data->ppDocument->doc_type_id == 2) ? 'Pre Sanction' : 'Post Sanction' }} )</b>
                             </a>
 
                         </div>
@@ -71,39 +72,17 @@
                             <table class="table  overview-table" cellpadding="0" cellspacing="0" border="1">
                                 <tbody>
                                     <tr>
-                                        @if($data->doc_id == '4')
-                                        <td width="20%"><b>Bank</b></td>
-                                        @endif
-                                        @if($data->doc_id == '5')
-                                        <td width="20%"><b>Finance Year</b></td>
-                                        @endif
-                                        @if($data->doc_id == '6')
-                                        <td width="20%"><b>GST Month-Year</b></td>
-                                        @endif
                                         <td width="20%"><b>File Name </b></td>
                                         <td width="20%"><b>Upload On </b></td>
-                                        @if($data->doc_id == '35' || $data->doc_id == '36')
                                         <td width="20%"><b>Comment </b></td>
-                                        @endif
                                         <td width="20%">Download</td>
                                         <td align="center" width="20%">Action</td>
                                     </tr>
                                     @foreach($documentData[$data->ppDocument->doc_name] as $value)
                                     <tr>
-                                        @if($data->doc_id == '4')
-                                        <td width="20%">{{ $value->doc_name }}</td>
-                                        @endif
-                                        @if($data->doc_id == '5')
-                                        <td width="20%">{{ $value->finc_year }}</td>
-                                        @endif
-                                        @if($data->doc_id == '6')
-                                        <td width="20%">{{ ($value->gst_month != '') ? date('M',$value->gst_month) : '' }}-{{ ($value->gst_year != '') ? $value->gst_year : '' }}</td>
-                                        @endif
                                         <td width="20%"> {{ (isset($value->userFile->file_name)) ? $value->userFile->file_name : ''}} </td>
                                         <td width="20%"> {{ (isset($value->created_at)) ? date('d-m-Y', strtotime($value->created_at)) : ''}} </td>
-                                        @if($data->doc_id == '35' || $data->doc_id == '36')
                                         <td width="20%"> {{ (isset($value->comment)) ? $value->comment : ''}} </td>
-                                        @endif
                                         <td width="20%"><a title="Download Document" href="{{ Storage::url($value->userFile->file_path) }}" download="{{ $value->userFile->file_name }}"><i class="fa fa-download"></i></a></td>
                                         <td align="center" width="20%">
                                             @if(request()->get('view_only'))
--- resources/views/backend/document/upload_document.blade.php
+++ resources/views/backend/document/upload_document.blade.php
@@ -9,73 +9,6 @@
 
         <div class="modal-body text-left">
 
-            <div id="is_required_addl_info">
-              <div class="row" id="bank_dates">
-                <div class="col-6">
-                   <div class="form-group">
-                      <label for="email">Select Month</label>
-                      <select class="form-control" name="bank_month">
-                         <option selected diabled value=''>Select Month</option>
-                         @for($i=1;$i<=12;$i++)
-                              <option value="{{$i}}">{{date('F', strtotime("2019-$i-01"))}}</option>
-                         @endfor
-                      </select>
-                   </div>
-                </div>
-                <div class="col-6">
-                   <div class="form-group">
-                      <label for="email">Select Year</label>
-                      <select class="form-control" name="bank_year">
-                         <option value=''>Select Year</option>
-                        @for($i=-3;$i<=0;$i++)
-                            <option>{{date('Y')+$i}}</option>
-                       @endfor;
-                      </select>
-                   </div>
-                </div>
-             </div>
-              <div class="form-group">
-                  <label for="email">Select Bank Name</label>
-                  <select class="form-control" name="file_bank_id">
-                      <option disabled value="" selected>Select Bank Name</option>
-                      
-                  </select>
-              </div>
-              <div class="form-group">
-                  <label for="email">Select Financial  Year</label>
-                  <select class="form-control" name="finc_year">
-                     <option value=''>Select Year</option>
-                     @for($i=-3;$i<=0;$i++)
-                          <option>{{ (date('Y') + ($i-1)).'-'.(date('Y') + $i) }}</option>
-                     @endfor;
-                  </select>
-               </div>
-              <div class="row">
-                  <div class="col-6">
-                     <div class="form-group">
-                        <label for="email">Select GST Month</label>
-                        <select class="form-control" name="gst_month">
-                           <option selected diabled value=''>Select Month</option>
-                           @for($i=1;$i<=12;$i++)
-                                <option value="{{$i}}">{{date('F', strtotime("2019-$i-01"))}}</option>
-                           @endfor
-                        </select>
-                     </div>
-                  </div>
-                  <div class="col-6">
-                     <div class="form-group">
-                        <label for="email">Select GST Year</label>
-                        <select class="form-control" name="gst_year">
-                           <option value=''>Select Year</option>
-                          @for($i=-3;$i<=0;$i++)
-                              <option>{{date('Y')+$i}}</option>
-                         @endfor;
-                        </select>
-                     </div>
-                  </div>
-               </div>
-            </div>
-
             <div class="custom-file upload-btn-cls mb-3 mt-2">
                 <input type="file" class="custom-file-input getFileName doc_file" id="doc_file" name="doc_file[]" multiple="">
                 <label class="custom-file-label" for="customFile">Choose file</label>
@@ -83,41 +16,9 @@
             <div class="row">
                 <div class="col-md-12">
                    <div class="form-group">
-                      <label for="email">Comment *</label>
+                      <label >Comment *</label>
                       <textarea type="text" name="comment" value="" class="form-control" tabindex="1" placeholder="Enter comment here ." required=""></textarea>
                    </div>
-                </div>
-            </div>
-            <div class="row" id="is_not_for_gst">
-              <div class="col-6">
-                 <label>Is Password Protected</label>
-                 <div class="form-group">
-                    <label for="is_password_y">
-                      <input type="radio" name="is_pwd_protected" id="is_password_y" value="1"> Yes
-                    </label>
-                    <label for="is_password_n">
-                      <input type="radio" name="is_pwd_protected" checked id="is_password_n" value="0"> No
-                    </label>
-                 </div>
-              </div>
-              <div class="col-6">
-                 <label>Is Scanned</label>
-                 <div class="form-group">
-                    <label for="is_scanned_y">
-                      <input type="radio" name="is_scanned" id="is_scanned_y" value="1"> Yes
-                    </label>
-                    <label for="is_scanned_n">
-                      <input type="radio" name="is_scanned" checked id="is_scanned_n" value="0"> No
-                    </label>
-                 </div>
-              </div>
-            </div>
-            <div class="row" id="password_file_div">
-                <div class="col-md-12">
-                    <div class="form-group">
-                        <label for="pwd_txt">Enter File Password</label>
-                        <input type="password" placeholder="Enter File Password" class="form-control" name="pwd_txt" id="pwd_txt">
-                     </div>
                 </div>
             </div>
             <button type="submit" class="btn btn-success float-right btn-sm" id="savedocument" >Submit</button>  
@@ -135,37 +36,8 @@
    $(document).ready(function(){
         var docId = parent.$('#uploadDocId').val();
         $('#myModal').modal('show');
-        $('#password_file_div').hide();
-        $('#is_not_for_gst').show();
         $('input[name=docId]').val(docId);
         $('input[name=doc_id]').val(docId);
-        $('select[name=file_bank_id]').parent('div').hide();
-        $('select[name=finc_year]').parent('div').hide();
-        $('select[name=gst_month]').parent('div').hide();
-        $('select[name=gst_year]').parent('div').hide();
-        $('textarea[name=comment]').parent('div').hide();
-        if (docId != 6 && $('input[name="is_pwd_protected"]').is(':checked') && $('input[name="is_pwd_protected"]:checked').val() == '1') {
-            $('#password_file_div').show();
-        }
-        $('#bank_dates').hide();
-        
-        if(docId == 4) {
-            $('select[name=file_bank_id]').parent('div').show();
-            $('#bank_dates').show();
-        } else if (docId == 5) {
-            $('select[name=finc_year]').parent('div').show();
-        } else if (docId == 6) {    
-            $('#is_not_for_gst').hide();
-            $('select[name=gst_month]').parent('div').show();
-            $('select[name=gst_year]').parent('div').show();            
-        } else if (docId == 35 || docId == 36) {
-            $('#is_not_for_gst').hide();    
-            $('textarea[name=comment]').parent('div').show();
-        } else {            
-            $('#is_not_for_gst').hide();
-            $('#is_required_addl_info').hide();       
-            $('#bank_dates').hide();       
-        }
         
     });
 </script>
