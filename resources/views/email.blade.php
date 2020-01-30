116bbf185ab3451887d2234082d6d42a41c31a2a
pankaj.sharma <pankaj.sharma@prolitus.com>
2020-01-30 14:57:37 +0530

    999

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header" style=" text-align: center;">
         <a href="#">
-            <img src="{{ asset('frontend/assets/images/logo.png') }}"  alt="Rent Alpha" width="200px;" />
+            <img src="{{ asset('frontend/assets/images/email_logo.png') }}"  alt="Rent Alpha" width="200px;" />
 
         </a>
     </td>
