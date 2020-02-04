7e178e7b8e52fa113cc1fc163043b8956770ce9c
root <hirdesh@prolitus.com>
2020-02-04 10:35:27 +0000

    changing permission

--- routes/reverseproxy.php
+++ routes/reverseproxy.php
@@ -4,9 +4,9 @@ if(config('app.env') == "production"){
     $proxy_url    = 'https://admin.capsavefinance.com';
     $proxy_schema = 'https';
 
-    dd(config('app.env'));
-    $proxy_url    = getenv('PROXY_URL');
-    $proxy_schema = getenv('PROXY_SCHEMA');
+//    dd(config('app.env'));
+ //   $proxy_url    = getenv('PROXY_URL');
+ //   $proxy_schema = getenv('PROXY_SCHEMA');
 
     if (!empty($proxy_url)) {
     \URL::forceRootUrl($proxy_url);
