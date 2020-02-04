5459fd181aab4abef6bcdae308a7c1e73f0819d2
kuldeep.singh <kuldeep.singh@prolitus.com>
2020-02-04 14:55:43 +0530

    reverse proxy

---
+++ routes/reverseproxy.php
@@ -0,0 +1,19 @@
+<?php
+
+if(config('app.env') == "production"){
+    $proxy_url    = 'https://admin.capsavefinance.com';
+    $proxy_schema = 'https';
+
+    dd(config('app.env'));
+    $proxy_url    = getenv('PROXY_URL');
+    $proxy_schema = getenv('PROXY_SCHEMA');
+
+    if (!empty($proxy_url)) {
+    \URL::forceRootUrl($proxy_url);
+    }
+
+    if (!empty($proxy_schema)) {
+    \URL::forceScheme($proxy_schema);
+    }
+}
+
