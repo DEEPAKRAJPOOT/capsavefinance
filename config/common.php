2f19469699455d6b7e2ffd14e1157bbff1ebf798
brajesh.kumar <brajesh.kumar@prolitus.com>
2020-02-04 11:35:05 +0530

    update

--- config/common.php
+++ config/common.php
@@ -10,9 +10,9 @@ return [
     'DOCUMENT_LIMIT' => '5',
     'RESEARCH_FORM_LIMIT' => '5',
     'AWARD_FORM_LIMIT' => '5',
-    'FRONTEND_FROM_EMAIL' => 'admin@dexter.com',
+    'FRONTEND_FROM_EMAIL' => 'admin@capsave.com',
     'FRONTEND_FROM_EMAIL_NAME' => 'Rent Alpha',
-    'BACKEND_FROM_EMAIL' => 'admin@dexter.com',
+    'BACKEND_FROM_EMAIL' => 'admin@capsave.com',
     'BACKEND_FROM_EMAIL_NAME' => 'Rent Alpha',
     'APISecret' => env('APISecret', ''),
     'apiKey' => env('apiKey', ''),
@@ -98,5 +98,6 @@ return [
         'OFFER_GENERATED'=>28,
         'DISBURSED'=>27,
     ],
+    'SEND_MAIL_ACTIVE'=>1,
    'SEND_MAIL'=>["gaurav.agarwal@prolitus.com", "varun.dudani@zuron.in", "binay.kumar@prolitus.com"],
 ];
