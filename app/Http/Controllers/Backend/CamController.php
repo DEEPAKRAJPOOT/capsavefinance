b72025df335c1c3ef7ac10d484c66d0c64a63896
Shivam Singh <shivam.singh@prolitus.com>
2020-02-11 17:15:13 +0530

    sadf

--- app/Http/Controllers/Backend/CamController.php
+++ app/Http/Controllers/Backend/CamController.php
@@ -243,11 +243,9 @@ class CamController extends Controller
       $leaseOfferData = array();
       $leaseOfferData = AppProgramOffer::getAllOffers($appId, '3');
       $arrStaticData = array();
-      $facilityTypeList= $this->mstRepo->getFacilityTypeList()->toarray();
-
       $arrStaticData['rentalFrequency'] = array('1'=>'Yearly','2'=>'Bi-Yearly','3'=>'Quarterly','4'=>'Monthly');
-      $arrStaticData['rentalFrequencyForPTPQ'] = array('1'=>'Year','2'=>'Bi-Year','3'=>'Quarter','4'=>'Months');
-      $arrStaticData['securityDepositType'] = array('1'=>'%','2'=>'INR');
+      $arrStaticData['rentalFrequencyForPTPQ'] = array('1'=>'Year','2'=>'Bi-Yearly','3'=>'Quarter','4'=>'Months');
+      $arrStaticData['securityDepositType'] = array('1'=>'&#8377;','2'=>'%');
       $arrStaticData['securityDepositOf'] = array('1'=>'Loan Amount','2'=>'Asset Value','3'=>'Asset Base Value','4'=>'Sanction');
       $arrStaticData['rentalFrequencyType'] = array('1'=>'Advance','2'=>'Arrears');
       $reviewerSummaryData = CamReviewerSummary::where('biz_id','=',$bizId)->where('app_id','=',$appId)->first();        
