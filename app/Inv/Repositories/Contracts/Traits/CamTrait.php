2b248ee7e4af1bffb506f0971d538214453d20d3
Shivam Singh <shivam.singh@prolitus.com>
2020-02-11 15:04:53 +0530

    change

--- app/Inv/Repositories/Contracts/Traits/CamTrait.php
+++ app/Inv/Repositories/Contracts/Traits/CamTrait.php
@@ -28,7 +28,7 @@ trait CamTrait
             $arrStaticData = array();
             $arrStaticData['rentalFrequency'] = array('1'=>'Yearly','2'=>'Bi-Yearly','3'=>'Quarterly','4'=>'Monthly');
             $arrStaticData['rentalFrequencyForPTPQ'] = array('1'=>'Year','2'=>'Bi-Year','3'=>'Quarter','4'=>'Months');
-            $arrStaticData['securityDepositType'] = array('1'=>'%','2'=>'INR');
+            $arrStaticData['securityDepositType'] = array('1'=>'INR','2'=>'%');
             $arrStaticData['securityDepositOf'] = array('1'=>'Loan Amount','2'=>'Asset Value','3'=>'Asset Base Value','4'=>'Sanction');
             $arrStaticData['rentalFrequencyType'] = array('1'=>'Advance','2'=>'Arrears');
 
@@ -87,7 +87,7 @@ trait CamTrait
                 }
 
                 return [
-                    'arrCamData' =>$arrCamData ,
+                    'arrCamData' =>$arrCamData,
                     'arrBizData' => $arrBizData, 
                     'reviewerSummaryData' => $reviewerSummaryData,
                     'arrHygieneData' => $arrHygieneData,
