1695424c4954c491fa2291464fd59097913fb4d0
gajendra <gajendra.singh@prolitus.com>
2020-02-25 20:45:46 +0530

    change

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -3225,7 +3225,7 @@ if ($err) {
                 $final_amount = 0; 
                 
             }
-            return response()->json(['status' => 1,'limit_amount' =>str_replace(".", "", $amountSum),'charge_amount' => str_replace(".", "", $getPercentAmount),'gst_amount' => str_replace(".", "", $final_amount)]); 
+            return response()->json(['status' => 1,'limit_amount' =>$amountSum,'charge_amount' => $getPercentAmount,'gst_amount' => $final_amount]); 
           }
           else
           {
@@ -3234,8 +3234,8 @@ if ($err) {
                   /* apply GST on percentage amount  ****/
                 $getAfterGstAmount = 0;
                 $final_amount = 0; 
-                   return response()->json(['status' => 0,'limit_amount' =>str_replace(".", "", $amountSum),'charge_amount' => str_replace(".", "", $getPercentAmount),'gst_amount' => str_replace(".", "", $final_amount)]); 
-         
+                     return response()->json(['status' => 0,'limit_amount' =>$amountSum,'charge_amount' => $getPercentAmount,'gst_amount' => $final_amount]); 
+        
          
           }
           
