9ed9e9ec910b125afefa27f445b899b4086db305
gajendra <gajendra.singh@prolitus.com>
2020-02-25 18:19:37 +0530

    change

--- app/Inv/Repositories/Models/User.php
+++ app/Inv/Repositories/Models/User.php
@@ -12,7 +12,7 @@ use App\Inv\Repositories\Models\Master\Role as Role;
 use App\Inv\Repositories\Models\Master\Permission;
 use App\Inv\Repositories\Models\Application;
 use App\Inv\Repositories\Models\AppProgramLimit;
-
+use App\Inv\Repositories\Models\AppProgramOffer;
 
 
 class User extends Authenticatable
@@ -622,7 +622,7 @@ class User extends Authenticatable
      public static function getProgramUser($user_id)
     {
          $appIds = Application::where('user_id', $user_id)->pluck('app_id');
-         $proId =  AppProgramLimit::whereHas('supplyOffers')->whereIn('app_id', $appIds)->pluck('prgm_id');
+         $proId =  AppProgramOffer::whereIn('app_id', $appIds)->where(['is_active' =>1,'is_approve' =>1,'status' =>1])->where('prgm_id','<>', null)->pluck('prgm_id');
          return Program::whereIn('prgm_id', $proId)->get();
       }
     
