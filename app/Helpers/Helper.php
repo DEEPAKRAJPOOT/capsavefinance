54caa0b2dfbdf3d95e7599a562f771bc010ca86c
Sudesh Kumar <sudesh.kumar@prolitus.com>
2020-02-27 18:25:59 +0530

    working

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -931,7 +931,7 @@ class Helper extends PaypalHelper
             $emailData['receiver_user_name'] = $user->f_name .' '. $user->m_name .' '. $user->l_name;
             $emailData['receiver_role_name'] = '';//$user->roles[0]->name;
             $emailData['receiver_email'] = $user->email;
-            $emailData['cover_note'] = $reviewerSummaryData->cover_note;
+            $emailData['cover_note'] = (isset($reviewerSummaryData->cover_note))?$reviewerSummaryData->cover_note:'';
             \Event::dispatch("APPLICATION_APPROVER_MAIL", serialize($emailData));
         }
         return $approvers;
