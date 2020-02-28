9a7f8708f3bab52cfabd53a8eea3fa5c16e7a7f2
pankaj.sharma <pankaj.sharma@prolitus.com>
2020-02-28 15:22:43 +0530

    conflict resolved

--- app/Inv/Repositories/Models/AppProgramOffer.php
+++ app/Inv/Repositories/Models/AppProgramOffer.php
@@ -347,9 +347,7 @@ class AppProgramOffer extends BaseModel {
        return AppProgramOffer::where(['app_id' => $arr->app_id,'is_approve'=> 1,'status' =>1,'is_approve' =>1])->sum('prgm_limit_amt');
       
     }
-      public function anchor(){
-        return $this->belongsTo('App\Inv\Repositories\Models\Anchor','anchor_id','anchor_id');
-    }
+
     
     public function anchor(){
         return $this->belongsTo('App\Inv\Repositories\Models\Anchor','anchor_id','anchor_id');
