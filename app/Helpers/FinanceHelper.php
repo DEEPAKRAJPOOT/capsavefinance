456725f4053ba13a42c744ee3565a55d61f223d3
kuldeep.singh <kuldeep.singh@prolitus.com>
2020-03-06 13:31:42 +0530

    fin tally

--- app/Helpers/FinanceHelper.php
+++ app/Helpers/FinanceHelper.php
@@ -6,16 +6,28 @@ class FinanceHelper {
 
     private $finRepo;
     private $jeConfigData;
+    private $jiConfigData;
     private $resp = [];
     public function __construct($finRepo = null) {
         $this->finRepo = $finRepo;
     }
 
-    public function finExecution($transConfigId = null, $userId = null, $appId = null, $bizId = null){
+    public function finExecution($transConfigId = null, $userId = null, $appId = null, $bizId = null) {
         try{
             if(isset($transConfigId) && !empty($transConfigId)) {
                 $this->jeConfigData = $this->finRepo->getAllJeConfigByTransConfigId($transConfigId); 
                 if(isset($this->jeConfigData) && !empty($this->jeConfigData)) {
+                    foreach($this->jeConfigData as $key=>$val) {                        
+                        $this->jiConfigData = $this->finRepo->getAllJiConfig($val->je_config_id);
+                        if(isset($this->jiConfigData) && !empty($this->jiConfigData)) {
+                            foreach($this->jiConfigData as $jikey=>$jival) {  
+                                print_r($jival);
+                            }                            
+                        } else {
+                            $this->resp['success'] = true;
+                            $this->resp['errorMsg'] = 'Ji Configuration not found';
+                        }                        
+                    }
                     dd($this->jeConfigData);
                     $this->resp['success'] = true;
                     $this->resp['errorMsg'] = '';
