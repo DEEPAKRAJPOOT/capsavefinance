37659605d1ce8dd59b4a79f57414fb836a158d56
pankaj.sharma <pankaj.sharma@prolitus.com>
2020-03-06 20:07:10 +0530

    999

--- public/common/js/iframePopup.js
+++ public/common/js/iframePopup.js
@@ -89,7 +89,7 @@ var iframe_ids = [
         'viewInterestAccrual',
         'manageUserRolePassword',
         'addBaseRateFrame',
-        'editBaseRateFrame'
+        'editBaseRateFrame',
         'previewSupplyChainSanctionLetter',
     ];
 
