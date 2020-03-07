328521eb5a2786a9add6dc862f442fa812a03b2e
akash <akash.kumar@prolitus.com>
2020-03-07 19:17:36 +0530

    changes

--- app/Inv/Repositories/Entities/User/UserRepository.php
+++ app/Inv/Repositories/Entities/User/UserRepository.php
@@ -1447,7 +1447,7 @@ class UserRepository extends BaseRepositories implements UserInterface
      */
     public function lmsGetCustomers()
     {
-        $result = LmsUser::with('user')->get();
+        $result = LmsUser::with('user')->orderBy('DESC')->get();
         return $result ?: false;
     }
 
