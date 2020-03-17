7bef96b9304161f2972db7242a3b5eca2ba5ac23
kuldeep.singh <kuldeep.singh@prolitus.com>
2020-03-17 14:29:05 +0530

    hot fixes 16032020

--- resources/views/backend/app/sanction_letter.blade.php
+++ resources/views/backend/app/sanction_letter.blade.php
@@ -865,9 +865,9 @@
                      </form>
                   </div>
                   <div id="SanctionLeasing" class="tab-pane fade">
-                     <div class="card card-color mb-0 {{empty($sanction_expire_msg) ? 'hide' : '' }}">
+                     <div class="card card-color mb-0 {{isset($sanction_expire_msg) && empty($sanction_expire_msg) ? 'hide' : '' }}">
                         <div class="card-header">
-                           <a class="card-title ">{{$sanction_expire_msg}}</a>
+                           <a class="card-title ">{{isset($sanction_expire_msg) ? $sanction_expire_msg : ''}}</a>
                         </div>
                      </div>
                      @if( is_array($offerData)?count($offerData):$offerData->count())
