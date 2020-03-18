4a1e3682ed7b694cc8a223ab9c15fad7f4471be2
Vishnu1996-hub <vishnu28496@gmail.com>
2020-03-18 16:02:52 +0530

    1234567890

--- resources/views/master/charges/add_charges.blade.php
+++ resources/views/master/charges/add_charges.blade.php
@@ -1,134 +1,136 @@
 @extends('layouts.backend.admin_popup_layout')
 @section('content')
 
- <div class="modal-body text-left">
-     <form id="chargesForm" name="chargesForm" method="POST" action="{{route('save_charges')}}" target="_top">
-              @csrf
+<div class="modal-body text-left">
+    <form id="chargesForm" name="chargesForm" method="POST" action="{{route('save_charges')}}" target="_top">
+        @csrf
 
-      <div class="row">
-        <div class="form-group col-md-12">
-          <label for="chrg_name">Charge Name</label>
-          <input type="text" class="form-control" id="chrg_name" name="chrg_name" placeholder="Enter Charge Name" maxlength="50">
-          <!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
-        </div>
-      </div>
-
-      <div class="row">
-         <div class="form-group col-md-12">
-            <label for="chrg_type">Charge Description</label>
-            <textarea class="form-control" id="chrg_desc" name="chrg_desc" placeholder="Charge Description" maxlength="500"></textarea>
-        </div>
-      </div>
-              
-      <div class="row">
-         <div class="form-group col-md-12">
-            <label for="chrg_type">Credit Description</label>
-            <textarea class="form-control" id="credit_desc" name="credit_desc" placeholder="Credit Description" maxlength="200"></textarea>
-        </div>
-      </div>
         <div class="row">
-         <div class="form-group col-md-12">
-            <label for="chrg_type">Debit Description</label>
-            <textarea class="form-control" id="debit_desc" name="debit_desc" placeholder="Debit Description" maxlength="200"></textarea>
+            <div class="form-group col-md-6">
+                <label for="chrg_name">Charge Name</label>
+                <input type="text" class="form-control" id="chrg_name" name="chrg_name" placeholder="Enter Charge Name" maxlength="50">
+                <!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
+            </div>
+            <!--      </div>
+            
+                  <div class="row">-->
+            <div class="form-group col-md-6">
+                <label for="chrg_type">Charge Description</label>
+                <textarea class="form-control" id="chrg_desc" name="chrg_desc" placeholder="Charge Description" maxlength="500" style="height:35px;"></textarea>
+            </div>
         </div>
-      </div>       
 
-      <div class="row">
-        
-        <div class="form-group col-md-6">
-             <label for="chrg_type">Charge Calculation</label><br />
-             <div class="form-check-inline ">
-               <label class="form-check-label fnt">
-               <input type="radio" class="form-check-input" name="chrg_calculation_type" value="1">Fixed
-               </label>
+        <div class="row">
+            <div class="form-group col-md-6">
+                <label for="chrg_type">Credit Description</label>
+                <textarea class="form-control" id="credit_desc" name="credit_desc" placeholder="Credit Description" maxlength="200" style="height:35px;"></textarea>
             </div>
-            <div class="form-check-inline">
-               <label class="form-check-label fnt">
-               <input type="radio" class="form-check-input" checked name="chrg_calculation_type" value="2">Percentage
-               </label>
+            <!--      </div>
+                    <div class="row">-->
+            <div class="form-group col-md-6">
+                <label for="chrg_type">Debit Description</label>
+                <textarea class="form-control" id="debit_desc" name="debit_desc" placeholder="Debit Description" maxlength="200" style="height:35px;"></textarea>
+            </div>
+        </div>       
+
+        <div class="row">
+            <div class="form-group col-md-6">
+                <div class="row">
+                    <div class="form-group col-md-6">
+                        <label for="chrg_type">Charge Calculation</label><br />
+                        <div class="form-check-inline ">
+                            <label class="form-check-label fnt">
+                                <input type="radio" class="form-check-input" name="chrg_calculation_type" value="1">Fixed
+                            </label>
+                        </div>
+                        <div class="form-check-inline">
+                            <label class="form-check-label fnt">
+                                <input type="radio" class="form-check-input" checked name="chrg_calculation_type" value="2">Percentage
+                            </label>
+                        </div>
+                    </div>
+                    <div class="form-group col-md-6" id="approved_limit_div">
+                        <label for="chrg_type">Charge Applicable On</label>
+                        <select class="form-control" name="chrg_applicable_id" id="chrg_applicable_id" style="height:35px; margin-bottom: -20px">
+                            <option value="" selected>Select</option>
+                            <option value="1">Limit Amount</option>
+                            <option value="2">Outstanding Amount</option>
+                            <option value="3">Outstanding Principal</option>
+                            <option value="4">Outstanding Interest</option>
+                            <option value="5">Overdue Amount</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group col-md-6 float-md-right">
+                <label for="chrg_calculation_amt">Amount/Percent</label>
+                <input type="text" class="form-control" id="chrg_calculation_amt" name="chrg_calculation_amt" placeholder="Charge Calculation Amount" maxlength="10">
+            </div>
+
+        </div>
+        <div class="row">
+
+
+            <!--      </div>
+                 <div class="row">-->
+            <div class="form-group col-md-6">
+                <label for="chrg_type">Charge Type</label><br />
+                <div class="form-check-inline ">
+                    <label class="form-check-label fnt">
+                        <input type="radio" class="form-check-input" checked name="chrg_type" value="1">Auto
+                    </label>
+                </div>
+                <div class="form-check-inline">
+                    <label class="form-check-label fnt">
+                        <input type="radio" class="form-check-input" name="chrg_type" value="2">Manual
+                    </label>
+                </div>
+                <div class="form-check-inline">
+                    <label class="form-check-label fnt">
+                        <input type="radio" class="form-check-input" name="chrg_type" value="3">Both
+                    </label>
+                </div>
+            </div> 
+            <!--        </div>           
+                    <div class="row">-->
+            <div class="form-group col-md-6">
+                <label for="is_gst_applicable">GST Applicable</label><br />
+                <div class="form-check-inline">
+                    <label class="form-check-label fnt">
+                        <input type="radio" class="form-check-input" checked name="is_gst_applicable" value="1">Yes
+                    </label>
+                </div>
+                <div class="form-check-inline">
+                    <label class="form-check-label fnt">
+                        <input type="radio" class="form-check-input" name="is_gst_applicable" value="2">No
+                    </label>
+                </div>
             </div>
         </div>
-      </div>
-      <div class="row">
-         <div class="form-group col-md-6">
-             <label for="chrg_calculation_amt">Amount/Percent</label>
-             <input type="text" class="form-control" id="chrg_calculation_amt" name="chrg_calculation_amt" placeholder="Charge Calculation Amount" maxlength="10">
-         </div>
-         <div class="form-group col-md-6" id="approved_limit_div">
-             <label for="chrg_type">Charge Applicable On</label>
-              <select class="form-control" name="chrg_applicable_id" id="chrg_applicable_id">
-                  <option value="" selected>Select</option>
-                  <option value="1">Limit Amount</option>
-                  <option value="2">Outstanding Amount</option>
-                  <option value="3">Outstanding Principal</option>
-                  <option value="4">Outstanding Interest</option>
-                  <option value="5">Overdue Amount</option>
-              </select>
-         </div>
-      </div>
-     <div class="row">
-         <div class="form-group col-md-6">
-             <label for="chrg_type">Charge Type</label><br />
-             <div class="form-check-inline ">
-               <label class="form-check-label fnt">
-               <input type="radio" class="form-check-input" checked name="chrg_type" value="1">Auto
-               </label>
+        <div class="row">
+            <div class="form-group col-md-6">
+                <label for="chrg_type">Charge Trigger</label>
+                <select class="form-control" name="chrg_tiger_id" id="chrg_tiger_id">
+                    <option value="" selected>Select</option>
+                    <option value="1">Limit Assignment</option>
+                    <option value="2">First Invoice Disbursement</option>
+                </select>
             </div>
-            <div class="form-check-inline">
-               <label class="form-check-label fnt">
-               <input type="radio" class="form-check-input" name="chrg_type" value="2">Manual
-               </label>
-            </div>
-              <div class="form-check-inline">
-               <label class="form-check-label fnt">
-               <input type="radio" class="form-check-input" name="chrg_type" value="3">Both
-               </label>
-            </div>
-        </div> 
-  </div>           
-      <div class="row">
-         <div class="form-group col-md-6">
-             <label for="is_gst_applicable">GST Applicable</label><br />
-             <div class="form-check-inline">
-               <label class="form-check-label fnt">
-               <input type="radio" class="form-check-input" checked name="is_gst_applicable" value="1">Yes
-               </label>
-            </div>
-            <div class="form-check-inline">
-               <label class="form-check-label fnt">
-               <input type="radio" class="form-check-input" name="is_gst_applicable" value="2">No
-               </label>
+            <div class="form-group col-md-6">
+                <label for="chrg_type">Status</label><br />
+                <select class="form-control" name="is_active" id="is_active">
+                    <option value="" selected>Select</option>
+                    <option value="1">Active</option>
+                    <option value="2">In-Active</option>
+                </select>
             </div>
         </div>
-        <div class="form-group col-md-6" id="gst_div">
-             <label for="chrg_type">GST Percent</label>
-             <input type="text" class="form-control" name="gst_percentage" placeholder="GST Percentage" readonly="readonly" value="{{Config::get('payment.gst')}}">
+        <div class="row">
+            <div class="form-group col-md-12 text-right">
+                <input type="submit" class="btn btn-success btn-sm" name="add_charge" id="add_charge" value="Submit"/>
+            </div>
         </div>
-      </div>
-      <div class="row">
-         <div class="form-group col-md-6">
-             <label for="chrg_type">Charge Trigger</label>
-             <select class="form-control" name="chrg_tiger_id" id="chrg_tiger_id">
-                  <option value="" selected>Select</option>
-                  <option value="1">Limit Assignment</option>
-                  <option value="2">First Invoice Disbursement</option>
-              </select>
-        </div>
-        <div class="form-group col-md-6">
-             <label for="chrg_type">Status</label><br />
-             <select class="form-control" name="is_active" id="is_active">
-                  <option value="" selected>Select</option>
-                  <option value="1">Active</option>
-                  <option value="2">In-Active</option>
-              </select>
-        </div>
-      </div>
-      <div class="row">
-         <div class="form-group col-md-12 text-right">
-             <input type="submit" class="btn btn-success btn-sm" name="add_charge" id="add_charge" value="Submit"/>
-        </div>
-      </div>
-   </form>
+    </form>
 </div>
 @endsection
 @section('jscript')
@@ -136,41 +138,45 @@
     $(document).ready(function () {
 
         $(document).on('click', 'input[name="chrg_calculation_type"]', function (e) {
-          if ($(this).val() == '2') $('#approved_limit_div').show();
-          else $('#approved_limit_div').hide();
+            if ($(this).val() == '2')
+                $('#approved_limit_div').show();
+            else
+                $('#approved_limit_div').hide();
         })
 
         $(document).on('click', 'input[name="is_gst_applicable"]', function (e) {
-          if ($(this).val() == '1') $('#gst_div').show();
-          else $('#gst_div').hide();
+            if ($(this).val() == '1')
+                $('#gst_div').show();
+            else
+                $('#gst_div').hide();
         })
 
 
-        $('#chargesForm').validate({ // initialize the plugin
+        $('#chargesForm').validate({// initialize the plugin
             rules: {
-                'chrg_name' : {
-                    required : true,
+                'chrg_name': {
+                    required: true,
                 },
-                'chrg_desc' : {
-                    required : true,
+                'chrg_desc': {
+                    required: true,
                 },
-                 'credit_desc' : {
-                    required : true,
+                'credit_desc': {
+                    required: true,
                 },
-                 'debit_desc' : {
-                    required : true,
+                'debit_desc': {
+                    required: true,
                 },
-                'chrg_calculation_amt' : {
-                    required : true,
+                'chrg_calculation_amt': {
+                    required: true,
                 },
-                'gst_percentage' : {
-                    required : true,
+                'gst_percentage': {
+                    required: true,
                 },
-                'chrg_applicable_id' : {
-                    required : true,
+                'chrg_applicable_id': {
+                    required: true,
                 },
-                'is_active' : {
-                    required : true,
+                'is_active': {
+                    required: true,
                 },
             },
             messages: {
@@ -180,10 +186,10 @@
                 'chrg_desc': {
                     required: "Please enter Charge Description",
                 },
-                 'credit_desc': {
+                'credit_desc': {
                     required: "Please enter Credit Description",
                 },
-                 'debit_desc': {
+                'debit_desc': {
                     required: "Please enter Debit Description",
                 },
                 'chrg_calculation_amt': {
