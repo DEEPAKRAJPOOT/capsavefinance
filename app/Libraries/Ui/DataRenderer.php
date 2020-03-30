67bf9405675fef9bf6df3ea8d55a2db57add527c
gajendra <gajendra.singh@prolitus.com>
2020-03-30 13:06:55 +0530

    change

--- app/Libraries/Ui/DataRenderer.php
+++ app/Libraries/Ui/DataRenderer.php
@@ -960,10 +960,16 @@ class DataRenderer implements DataProviderInterface
                ->addColumn(
                     'action',
                     function ($invoice) {
-                    $action = "";
+                     $action = "";
+                     $id = Auth::user()->user_id;
+                     $role_id = DB::table('role_user')->where(['user_id' => $id])->pluck('role_id');
+                     $chkUser =    DB::table('roles')->whereIn('id',$role_id)->first();
+                     if( $chkUser->id!==11)
+                     {
                       $action .='<a title="Disbursed Que" data-status="9"  data-id="'.(($invoice->invoice_id) ? $invoice->invoice_id : '' ).'" class="btn btn-action-btn btn-sm approveInv"><i class="fa fa-share-square" aria-hidden="true"></i></a>';
                       $action .='</br></br><div class="d-flex"><select  data-id="'.(($invoice->invoice_id) ? $invoice->invoice_id : '' ).'" class=" btn-success rounded approveInv1"><option value="0">Change Status</option><option value="7">Pending</option><option value="14">Reject</option></select></div>';
-                    return  $action;
+                     }
+                      return  $action;
                 })
                  ->filter(function ($query) use ($request) {
                   
@@ -1116,12 +1122,15 @@ class DataRenderer implements DataProviderInterface
                         }
                          $expl  =  explode(",",$invoice->program->invoice_approval); 
                          $action = "";
+                    if( $chkUser->id!==11)
+                     {   
                          $action .='</br><div class="d-flex"><select  data-id="'.(($invoice->invoice_id) ? $invoice->invoice_id : '' ).'" class=" btn-success rounded approveInv1"><option value="0">Change Status</option><option value="7">Pending</option>';
                        if(in_array($customer, $expl)) 
                        {
                          $action .='<option value="8">Approve</option>';
                        }
                         $action .='<option value="14">Reject</option></select></div>';
+                     }    
                         return  $action;
                 })
                  ->filter(function ($query) use ($request) {
@@ -1349,13 +1358,18 @@ class DataRenderer implements DataProviderInterface
                         }
                          $expl  =  explode(",",$invoice->program->invoice_approval); 
                        $action = "";
+                      if( $chkUser->id!=11)
+                      {  
                        $action .= '<div class="d-flex"><select  data-id="'.(($invoice->invoice_id) ? $invoice->invoice_id : '' ).'" class=" btn-success rounded approveInv"><option value="0">Change Status</option>';
                        if(in_array($customer, $expl)) 
                        {
                         $action .='<option value="8">Approve</option>';
                        }
                         $action .= '<option value="9">Disb Que</option></select>&nbsp;&nbsp;<a data-toggle="modal"  data-target="#modalInvoiceFailed" data-height="400px" data-width="100%" accesskey="" data-url ="'.route("invoice_failed_status",["invoice_id" => $invoice->invoice_id]).'"> <button class="btn-upload btn-sm" type="button" title="View Failed Disbursement"> <i class="fa fa-eye"></i></button></a></div>';
-                      return $action;
+                      }  
+                        $action .= '&nbsp;&nbsp;<a data-toggle="modal"  data-target="#modalInvoiceFailed" data-height="400px" data-width="100%" accesskey="" data-url ="'.route("invoice_failed_status",["invoice_id" => $invoice->invoice_id]).'"> <button class="btn-upload btn-sm" type="button" title="View Failed Disbursement"> <i class="fa fa-eye"></i></button></a></div>';
+                     
+                        return $action;
                 })
                  ->filter(function ($query) use ($request) {
                   
@@ -1697,6 +1711,8 @@ class DataRenderer implements DataProviderInterface
                         }
                          $expl  =  explode(",",$invoice->program->invoice_approval); 
                        $action = "";
+                       if( $chkUser->id!=11)
+                      { 
                        $action .= '<div class="d-flex"><select  data-id="'.(($invoice->invoice_id) ? $invoice->invoice_id : '' ).'" class=" btn-success rounded approveInv"><option value="0">Change Status</option>';
                        $action .= '<option value="7">Pending</option>';
                        if(in_array($customer, $expl)) 
@@ -1704,6 +1720,7 @@ class DataRenderer implements DataProviderInterface
                         $action .='<option value="8">Approve</option>';
                        }
                         $action .='</select></div>';
+                      }
                         return $action;
 
                 })
