4b3bac65226a59cab555c2c178d85365a2b6cbaf
Ravi prakash <ravi.awasthi@prolitus.com>
2020-03-31 17:38:04 +0530

    9876543210

--- app/Http/Controllers/Backend/FinanceController.php
+++ app/Http/Controllers/Backend/FinanceController.php
@@ -12,6 +12,12 @@ use App\Http\Requests\Backend\CreateJiConfigRequest;
 use App\Http\Requests\Backend\CreateJournalRequest;
 use App\Http\Requests\Backend\CreateAccountRequest;
 use App\Helpers\FinanceHelper;
+use PHPExcel;
+use PHPExcel_IOFactory;
+use PHPExcel_Style_Fill;
+use PHPExcel_Cell_DataType;
+use PHPExcel_Style_Alignment;
+use PHPExcel_Style_Border;
 
 class FinanceController extends Controller {
 
@@ -64,8 +70,16 @@ class FinanceController extends Controller {
     }
     
     public function exportTransactions(Request $request) {
+        // $voucher_type = $request->get('type');
+        // if (!in_array($voucher_type, [1,2])) {
+        //     Session::flash('error','Invalid voucher type found.');
+        //     return back();
+        // }
+        // 1 for journal and 2 for bank
+        $where = [];
         $result = $this->finRepo->getTally();
-         $records[] = [
+         $records = [];
+         $header[] = [
             "transaction_id" => "transaction Id",
             "batch_id" => "Batch Id",
             "amount" => "Txn Amount",
@@ -93,13 +107,16 @@ class FinanceController extends Controller {
             "pay_from" => "Pay From",
             "txn_id" => "Txn Id",
             "comment" => "Narration",
-            "created_at" => "created_at",
+            "created_at" => "created At",
             "created_by" => "Created By",
             ];
         foreach ($result as $key => $value) {
            $records[] = (array)$value;
         }
-        $this->array_to_csv($records, "execl.csv");
+        $data = $header + $records;
+        $toExportData = ['JOURNAL'=> $data, 'PAYMENT' => $data];
+        $this->array_to_excel($toExportData, "execl.xlsx");
+        // $this->array_to_csv($toExportData, "execl.csv");
     }
 
     public function crateJeConfig(Request $request) {
@@ -308,22 +325,157 @@ class FinanceController extends Controller {
         if ($download != ""){    
             header('Content-Type: application/csv');
             header('Content-Disposition: attachement; filename="' . $download . '"');
-        }        
+        }
         ob_start();
         $f = fopen('php://output', 'w') or die("Can't open php://output");
-        $n = 0;     
         foreach ($array as $line){
-            $n++;
-            if (!fputcsv($f, $line)){
+            if (!fputcsv($f, $line,"\t")){
                continue;
             }
         }
         $str = ob_get_contents();
+        fclose($f);
         ob_end_clean();
         if ($download == ""){
             return $str;    
         }else{    
             echo $str;
         }        
+    }
+
+    public function array_to_excel($toExportData, $file_name = "") {
+        ob_start();
+        // if(empty($file_name)) {
+            $file_name = "Report - " . _getRand(15).".xlsx";
+        // }
+        $activeSheet = 0;
+        $objPHPExcel = new PHPExcel();
+        foreach ($toExportData as $title => $data) {
+            $header_cols = array_values($data[0]);
+            unset($data[0]);
+            $sheetTitle = $title;
+            $objPHPExcel->createSheet();
+            $objPHPExcel->setActiveSheetIndex($activeSheet);
+            $activeSheet++;
+            $flag_array_intestazione = false;
+            $column = 0;
+            $header_row = 2;
+            $start_row = 4;
+            $row = $start_row;
+            $column = 0;
+            $flag_array_intestazione = true;
+            $floor = floor(count($header_cols)/26);
+            $reminder = count($header_cols) % 26;
+            $char = ($floor > 0 ? chr(ord("A") + $floor - 1) : '').chr(ord("A") + $reminder - 1);
+            foreach($data as $key => $item) {
+              foreach($item as $key1 => $item1) {
+                $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow($column, $row, $item1);
+                if($flag_array_intestazione) $array_intestazione[] = $key1;
+                $column++;
+              }
+              $argb = "FFFFFFFF";
+              if ($row % 2 == 1){
+                $argb = "FFE0E0E0";
+              }
+              $styleArray = array(
+                'fill' => array(
+                  'type' => PHPExcel_Style_Fill::FILL_SOLID,
+                  'startcolor' => array(
+                    'argb' => $argb,
+                  ),
+                ),
+              );
+              $objPHPExcel->getActiveSheet()->getStyle('A'. $row .':' . $char . $row)->applyFromArray($styleArray);
+              if($flag_array_intestazione) $flag_array_intestazione = false;
+              $column = 0;
+              $row++;
+            }
+            $end_row = $row - 1;
+            $row = $header_row;
+            $column = 0;
+            if (!empty($header_cols)) {
+                $array_intestazione = $header_cols;
+            }
+            foreach($array_intestazione as $key) {
+               $objPHPExcel->getActiveSheet()->getCellByColumnAndRow($column, $row)->setValueExplicit($key, PHPExcel_Cell_DataType::TYPE_STRING);
+                  $column++;
+            }
+            $styleArray = array(
+              'font' => array(
+                'bold' => true,
+              ),
+              'alignment' => array(
+              'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+              ),
+              'borders' => array(
+              'top' => array(
+              'style' => PHPExcel_Style_Border::BORDER_THIN,
+              ),
+              ),
+              'fill' => array(
+              'type' => PHPExcel_Style_Fill::FILL_GRADIENT_LINEAR,
+              'rotation' => 90,
+              'startcolor' => array(
+              'argb' => 'FFA0A0A0',
+              ),
+              'endcolor' => array(
+              'argb' => 'FFFFFFFF',
+              ),
+              ),
+            );
+     
+            $objPHPExcel->getActiveSheet()->mergeCells('A1:' . $char . '1');
+            $objPHPExcel->getActiveSheet()->setCellValue('A1', $file_name);
+            $objPHPExcel->getActiveSheet()->getStyle('A1')->applyFromArray($styleArray);
+     
+            $styleArray = array(
+              'font' => array(
+                'bold' => true,
+              ),
+              'alignment' => array(
+                'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+              ),
+              'borders' => array(
+                  'top' => array(
+                    'style' => PHPExcel_Style_Border::BORDER_THIN,
+                  ),
+              ),
+              'fill' => array(
+                'type' => PHPExcel_Style_Fill::FILL_SOLID,
+                'rotation' => 90,
+                'startcolor' => array(
+                    'argb' => 'FFA0A0A0',
+                ),
+                'endcolor' => array(
+                    'argb' => 'FFFFFFFF',
+                ),
+              ),
+            );
+            $objPHPExcel->getActiveSheet()->getStyle('A'. $header_row .':' . $char . $header_row)->applyFromArray($styleArray);
+     
+            foreach($array_intestazione as $key => $el) {
+                 $floor = floor(($key)/26);
+                 $reminder = ($key) % 26;
+                 $char = ($floor > 0 ? chr(ord("A") + $floor-1) : '').chr(ord("A") + $reminder);
+                 $objPHPExcel->getActiveSheet()->getColumnDimension($char)->setAutoSize(true);
+            }
+            $styleArray = array(
+              'borders' => array(
+                'allborders' => array(
+                  'style' => PHPExcel_Style_Border::BORDER_THIN,
+                  'color' => array('argb' => 'FFFF0000'),
+                ),
+              ),
+            );
+            $objPHPExcel->getActiveSheet()->getStyle('A'. $header_row .':' . $char . $end_row)->applyFromArray($styleArray);
+            $objPHPExcel->getActiveSheet()->setTitle($sheetTitle);
+        }
+        header('Content-Type: application/vnd.ms-excel');
+        header('Content-Disposition: attachment;filename="' . $file_name . '"');
+        header('Cache-Control: max-age=0');
+        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+        $objWriter->save('php://output');
+        ob_end_flush();
+        exit; 
     }
 }
