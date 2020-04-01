e6891c1c25fec3a303535fffa42c5ce9493db845
gajendra <gajendra.singh@prolitus.com>
2020-04-01 18:39:27 +0530

    chnge

--- resources/views/backend/invoice/upload_all_invoice.blade.php
+++ resources/views/backend/invoice/upload_all_invoice.blade.php
@@ -668,10 +668,12 @@ var messages = {
       var program_id =  $("#program_id").val(); 
       var anchor_id =  $("#anchor_id").val(); 
       var supplier_id  = $(this).val();
+       $("#invoice_date, #invoice_due_date").val(''); 
       if(supplier_id=='')
       {
           return false; 
       }
+    
      var postData =  ({'bulk':0,'anchor_id':anchor_id,'supplier_id':supplier_id,'program_id':program_id,'_token':messages.token});
        jQuery.ajax({
         url: messages.get_tenor,
