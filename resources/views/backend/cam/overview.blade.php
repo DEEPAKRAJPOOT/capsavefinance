213b595901d01116f79b5a818400cd9c3a402078
pankaj.sharma <pankaj.sharma@prolitus.com>
2020-04-02 16:28:18 +0530

    ckeditor upload

--- resources/views/backend/cam/overview.blade.php
+++ resources/views/backend/cam/overview.blade.php
@@ -371,10 +371,10 @@
    });
 
       var ckeditorOptions =  {
-        filebrowserUploadUrl: "{{route('upload_ckeditor_image', ['_token' => csrf_token() ])}}",
+        filebrowserUploadUrl: "{{route('upload_ckeditor_image', ['_token' => csrf_token(), 'type' => 'file' ])}}",
         filebrowserUploadMethod: 'form',
         //extraPlugins: 'imageuploader',
-        //filebrowserImageUploadUrl:"{{ route('upload_ckeditor_image') }}",
+        imageUploadUrl:"{{ route('upload_ckeditor_image', ['_token' => csrf_token(), 'type' => 'image' ]) }}",
         disallowedContent: 'img{width,height};'
       };
 
