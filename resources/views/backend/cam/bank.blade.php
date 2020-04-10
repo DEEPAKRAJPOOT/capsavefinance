cbb959667a1922439d38a104743999cf00d3ab0e
kuldeep.singh <kuldeep.singh@prolitus.com>
2020-04-10 16:16:04 +0530

    #3603 bank detail added

--- resources/views/backend/cam/bank.blade.php
+++ resources/views/backend/cam/bank.blade.php
@@ -105,10 +105,10 @@
                            <td class="" tabindex="0" rowspan="1" colspan="1" width="12%">Name of Bank/NBFC</td>
                            <td class="" tabindex="0" rowspan="1" colspan="1">Fund based Facility</td>
                            <td class="" tabindex="0" rowspan="1" colspan="1">Facility Amount(Rs. Mn)</td>
-                           <td class="" tabindex="0" rowspan="1" colspan="1">O/S as on <input type="text" class="col-md-8" value="{{$debtPosition->fund_date ?? '' }}" name="fund_date" id="fund_date" placeholder="Date"> (Rs. Mn)</td>
+                           <td class="" tabindex="0" rowspan="1" colspan="1">O/S as on <input type="text" class="col-md-10" value="{{$debtPosition->fund_date ?? '' }}" name="fund_date" id="fund_date" placeholder="Date"> (Rs. Mn)</td>
                            <td class="" tabindex="0" rowspan="1" colspan="1">Non-fund based Facility</td>
                            <td class="" tabindex="0" rowspan="1" colspan="1">Facility Amount(Rs. Mn)</td>
-                           <td class="" tabindex="0" rowspan="1" colspan="1">O/S as on <input type="text" class="col-md-8" value="{{$debtPosition->nonfund_date ?? '' }}" name="nonfund_date" id="nonfund_date" placeholder="Date"> (Rs. Mn)</td>
+                           <td class="" tabindex="0" rowspan="1" colspan="1">O/S as on <input type="text" class="col-md-10" value="{{$debtPosition->nonfund_date ?? '' }}" name="nonfund_date" id="nonfund_date" placeholder="Date"> (Rs. Mn)</td>
                            <td class="" tabindex="0" rowspan="1" colspan="1">Length of Relationship</td>
                         </tr>  
 
