5e9a38effedbfbcf723d37c2971e4bf5d079e668
sudesh.kumar <sudesh.kumar@prolitus.com>
2020-04-17 09:53:48 +0530

    working

--- app/Inv/Repositories/Models/Lms/TransType.php
+++ app/Inv/Repositories/Models/Lms/TransType.php
@@ -117,12 +117,12 @@ class TransType extends BaseModel {
         array_push($trans_type,(int)config('lms.TRANS_TYPE.INTEREST_PAID'));
 
     
-        // $query = "SELECT t1.trans_type, t1.amount AS debit_amount, IFNULL(SUM(t2.amount), 0) as credit_amount, (t1.amount - IFNULL(SUM(t2.amount), 0)) as remaining FROM `get_all_charges` t1 LEFT JOIN rta_transactions as t2 ON t1.trans_id = t2.parent_trans_id WHERE t1.entry_type = 0  ". $cond ." GROUP BY t1.trans_id HAVING remaining > 0 "; 
-        // $result = \DB::SELECT(\DB::raw($query));
+        $query = "SELECT t1.trans_type, t1.amount AS debit_amount, IFNULL(SUM(t2.amount), 0) as credit_amount, (t1.amount - IFNULL(SUM(t2.amount), 0)) as remaining FROM `get_all_charges` t1 LEFT JOIN rta_transactions as t2 ON t1.trans_id = t2.parent_trans_id WHERE t1.entry_type = 0  ". $cond ." GROUP BY t1.trans_id HAVING remaining > 0 "; 
+        $result = \DB::SELECT(\DB::raw($query));
        
-        // foreach ($result as $key => $value) {
-        //     array_push($trans_type,$value->trans_type);
-        // }
+        foreach ($result as $key => $value) {
+            array_push($trans_type,$value->trans_type);
+        }
 
         return self::select('id','credit_desc','trans_name')
         ->where("is_visible","=", 1)
