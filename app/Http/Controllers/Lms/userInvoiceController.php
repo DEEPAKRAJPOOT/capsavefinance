7efce419d8b20166b5022a1f19cc80f53b977cfc
Ravi prakash <ravi.awasthi@prolitus.com>
2020-04-20 15:58:20 +0530

    aditya code

---
+++ app/Http/Controllers/Lms/userInvoiceController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Lms;
+
+use App\Http\Controllers\Controller;
+use Auth;
+use Illuminate\Http\Request;
+use App\Inv\Repositories\Contracts\MasterInterface;
+use App\Inv\Repositories\Contracts\UserInterface as InvUserRepoInterface;
+use App\Inv\Repositories\Contracts\ApplicationInterface as InvAppRepoInterface;
+use App\Inv\Repositories\Models\Master\State;
+use DB;
+use Session;
+use Helpers;
+// use App\Inv\Repositories\Contracts\Traits\ApplicationTrait;
+
+class userInvoiceController extends Controller
+{
+    //use ApplicationTrait;
+
+    protected $appRepo;
+    protected $userRepo;
+    protected $docRepo;
+    protected $master;
+
+    /**
+     * The pdf instance.
+     *
+     * @var App\Libraries\Pdf
+     */
+    protected $pdf;
+
+    public function __construct(InvAppRepoInterface $app_repo, InvUserRepoInterface $user_repo, MasterInterface $master)
+    {
+        $this->appRepo = $app_repo;
+        $this->userRepo = $user_repo;
+        //	$this->docRepo = $doc_repo;
+        $this->master = $master;
+        $this->middleware('checkBackendLeadAccess');
+    }
+
+    /**
+     * Display invoice as per User.
+     *
+     */
+    public function getUserInvoice(Request $request) 
+    {
+
+        
+        try {
+
+            $user_id = $request->get('user_id');
+            $userInfo = $this->userRepo->getCustomerDetail($user_id);
+
+            return view('lms.invoice.invoice_user_invoice')->with(['userInfo' => $userInfo]);
+        } catch (Exception $ex) {
+             return redirect()->back()->withErrors(Helpers::getExceptionMessage($ex));
+        }
+    }
+
+    /**
+     * Create invoice as per User.
+     *
+     */
+    public function createUserInvoice(Request $request) 
+    {
+
+        
+        try {
+
+            $user_id = $request->get('user_id');
+            $userInfo = $this->userRepo->getCustomerDetail($user_id);
+
+            $state_list = ['' => 'Please Select'] + $this->master->getAddStateList()->toArray();
+            return view('lms.invoice.create_user_invoice')->with(['user_id' => $user_id, 'state_list' => $state_list]);
+        } catch (Exception $ex) {
+             return redirect()->back()->withErrors(Helpers::getExceptionMessage($ex));
+        }
+    }
+
+}
