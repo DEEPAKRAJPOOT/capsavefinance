a7f1768483bb341f1c187f0bc1d5832c41b9ab3c
hyphen <hyphen.call@gmail.com>
2020-04-20 17:48:31 +0530

    sdf

--- app/Http/Controllers/Lms/userInvoiceController.php
+++ app/Http/Controllers/Lms/userInvoiceController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Inv\Repositories\Contracts\MasterInterface;
 use App\Inv\Repositories\Contracts\UserInterface as InvUserRepoInterface;
 use App\Inv\Repositories\Contracts\ApplicationInterface as InvAppRepoInterface;
+use App\Inv\Repositories\Contracts\UserInvoiceInterface as InvUserInvRepoInterface;
 use App\Inv\Repositories\Models\Master\State;
 use DB;
 use Session;
@@ -22,6 +23,7 @@ class userInvoiceController extends Controller
     protected $userRepo;
     protected $docRepo;
     protected $master;
+    protected $UserInvRepo;
 
     /**
      * The pdf instance.
@@ -30,10 +32,11 @@ class userInvoiceController extends Controller
      */
     protected $pdf;
 
-    public function __construct(InvAppRepoInterface $app_repo, InvUserRepoInterface $user_repo, MasterInterface $master) {
+    public function __construct(InvAppRepoInterface $app_repo, InvUserRepoInterface $user_repo, MasterInterface $master, InvUserInvRepoInterface $UserInvRepo) {
         $this->appRepo = $app_repo;
         $this->userRepo = $user_repo;
         $this->master = $master;
+        $this->UserInvRepo = $UserInvRepo;
         $this->middleware('checkBackendLeadAccess');
     }
 
@@ -59,6 +62,8 @@ class userInvoiceController extends Controller
         try {
             $user_id = $request->get('user_id');
             $userInfo = $this->userRepo->getCustomerDetail($user_id);
+            $app_id = $this->UserInvRepo->getAppsByUserId($user_id);
+            
             $state_list = ['' => 'Please Select'] + $this->master->getAddStateList()->toArray();
             return view('lms.invoice.create_user_invoice')->with(['user_id' => $user_id, 'state_list' => $state_list]);
         } catch (Exception $ex) {
