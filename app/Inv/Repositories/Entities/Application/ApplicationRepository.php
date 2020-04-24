d6e00f5ea87c6238acbd24ed5a7f74727b49a525
vishnu.kumar <vishnu.kumar@prolitus.com>
2020-04-24 18:14:19 +0400

    1234567890

--- app/Inv/Repositories/Entities/Application/ApplicationRepository.php
+++ app/Inv/Repositories/Entities/Application/ApplicationRepository.php
@@ -1695,9 +1695,9 @@ class ApplicationRepository extends BaseRepositories implements ApplicationInter
      * @param type $id int
      * @return type mixed
      */
-    public function isDefalutCmpBankAcc($attributes, $flag)
+    public function isDefalutCmpBankAcc($attributes, $is_default)
     {
-        return UserBankAccount::isDefalutCmpBankAcc($attributes, $flag);
+        return UserBankAccount::isDefalutCmpBankAcc($attributes, $is_default);
    }
 
 
