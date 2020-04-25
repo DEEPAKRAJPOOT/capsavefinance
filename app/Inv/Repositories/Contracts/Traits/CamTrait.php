4433c608c34d3e98b8a156b8c4c14cbdadb925ee
Ravi prakash <ravi.awasthi@prolitus.com>
2020-04-25 19:15:08 +0530

    9875432

--- app/Inv/Repositories/Contracts/Traits/CamTrait.php
+++ app/Inv/Repositories/Contracts/Traits/CamTrait.php
@@ -168,14 +168,15 @@ trait CamTrait
                     $negativeRiskCmntArr = array_filter($dataRiskComments, array($this, "filterRiskCommentNegative"));
                   }
                 } 
+
                 $supplyOfferData = $this->appRepo->getAllOffers($appId, 1);//for supply chain
                 foreach($supplyOfferData as $key=>$val){
                   $supplyOfferData[$key]['anchorData'] = $this->userRepo->getAnchorDataById($val->anchor_id)->pluck('f_name')->first();
                   $supplyOfferData[$key]['programData'] = $this->appRepo->getSelectedProgramData(['prgm_id' => $val->prgm_id], ['*'], ['programDoc', 'programCharges'])->first();
                   $supplyOfferData[$key]['subProgramData'] = $this->appRepo->getSelectedProgramData(['prgm_id' => $val->prgm_id, 'is_null_parent_prgm_id' => true], ['*'], ['programDoc', 'programCharges'])->first();
-                                                      dd($supplyOfferData[$key]['subProgramData'], "9876543210");
                 }
-
+                                                     
+                dd($supplyOfferData);
                 return [
                     'arrCamData' =>$arrCamData,
                     'arrBizData' => $arrBizData, 
