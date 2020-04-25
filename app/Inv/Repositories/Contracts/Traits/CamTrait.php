cd738100e27dc0ca0f509a1e293754894f8f7e3e
Ravi prakash <ravi.awasthi@prolitus.com>
2020-04-25 19:05:46 +0530

    987653210

--- app/Inv/Repositories/Contracts/Traits/CamTrait.php
+++ app/Inv/Repositories/Contracts/Traits/CamTrait.php
@@ -169,12 +169,12 @@ trait CamTrait
                   }
                 } 
                 $supplyOfferData = $this->appRepo->getAllOffers($appId, 1);//for supply chain
+                dd($supplyOfferData);
                 foreach($supplyOfferData as $key=>$val){
                   $supplyOfferData[$key]['anchorData'] = $this->userRepo->getAnchorDataById($val->anchor_id)->pluck('f_name')->first();
                   $supplyOfferData[$key]['programData'] = $this->appRepo->getSelectedProgramData(['prgm_id' => $val->prgm_id], ['*'], ['programDoc', 'programCharges'])->first();
                   $supplyOfferData[$key]['subProgramData'] = $this->appRepo->getSelectedProgramData(['prgm_id' => $val->prgm_id, 'is_null_parent_prgm_id' => true], ['*'], ['programDoc', 'programCharges'])->first();
                 }
-                dd($financeData, "98765432");
 
                 return [
                     'arrCamData' =>$arrCamData,
