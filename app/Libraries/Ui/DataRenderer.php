6127b8d5c39f1dc42588544680057bcb3bc1d864
pankaj.sharma <pankaj.sharma@prolitus.com>
2020-04-29 00:21:12 +0530

    #4033 checks on create multiple apps

--- app/Libraries/Ui/DataRenderer.php
+++ app/Libraries/Ui/DataRenderer.php
@@ -494,7 +494,7 @@ class DataRenderer implements DataProviderInterface
                     'assoc_anchor',
                     function ($app) {                        
                      if($app->anchor_id){
-                    $userInfo=User::getUserByAnchorId($app->anchor_id);
+                    $userInfo=User::getUserByAnchorId((int) $app->anchor_id);
                        $achorName= ($userInfo)? ucwords($userInfo->f_name.' '.$userInfo->l_name): 'NA';
                     }else{
                       $achorName='';  
@@ -1937,8 +1937,9 @@ class DataRenderer implements DataProviderInterface
                 ->addColumn(
                     'status',
                     function ($app) {
-                 return $app->status == 1 ? 'Completed' : 'Incomplete';
-
+                    //return $app->status == 1 ? 'Completed' : 'Incomplete';
+                    $app_status = config('common.app_status');                               
+                    return isset($app_status[$app->status]) ? $app_status[$app->status] : ''; 
                 })
                 ->addColumn(
                     'action',
