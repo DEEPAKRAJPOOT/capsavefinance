de7b948822b816ae502a866cb1343600a87006d8
Ravi prakash <ravi.awasthi@prolitus.com>
2020-05-05 20:47:22 +0530

    9876543210

--- app/Http/Controllers/Auth/ApiController.php
+++ app/Http/Controllers/Auth/ApiController.php
@@ -146,7 +146,7 @@ class ApiController
             if (in_array($txn->trans_type, [config('lms.TRANS_TYPE.TDS'), config('lms.TRANS_TYPE.REFUND'), config('lms.TRANS_TYPE.NON_FACTORED_AMT'), config('lms.TRANS_TYPE.WAVED_OFF'),  config('lms.TRANS_TYPE.MARGIN')]) && $txn->entry_type == 1) {
                $tally_voucher_type_id = 3;
             } 
-            if (in_array($txn->trans_type, [config('lms.TRANS_TYPE.TDS'), config('lms.TRANS_TYPE.REFUND'), config('lms.TRANS_TYPE.NON_FACTORED_AMT'), config('lms.TRANS_TYPE.WAVED_OFF'),  config('lms.TRANS_TYPE.MARGIN')]) && $txn->entry_type == 0) {
+            if (in_array($txn->trans_type, [config('lms.TRANS_TYPE.MARGIN')]) && $txn->entry_type == 0) {
                continue;
             }
             if ($txn->trans_type == 32 && $txn->entry_type == 0) {
