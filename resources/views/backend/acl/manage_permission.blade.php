7eb22e1f8af03dd30d5664aef1b15f6a47cde745
pankaj.sharma <pankaj.sharma@prolitus.com>
2020-05-18 12:42:35 +0530

    #4171 acl

--- resources/views/backend/acl/manage_permission.blade.php
+++ resources/views/backend/acl/manage_permission.blade.php
@@ -59,8 +59,6 @@
                                     @endif
                                     @endif
                                     
-                                    @if ($ParentData['display_name'] == 'Lead List') <div class="cls-perm-los"> @endif
-                                    @if ($ParentData['display_name'] == 'Invoice Management') <div class="cls-perm-lms"> @endif
                                     <div class="col-md-3">
                                     <ul>
                                         <li>
@@ -77,8 +75,6 @@
                                         </li>
                                     </ul>
                                     </div>
-                                    @if ($ParentData['display_name'] == 'Manage Co-lender') </div> @endif
-                                    @if ($ParentData['display_name'] == 'Tally') </div> @endif
                                     @endforeach
                                 </div>
                                 
