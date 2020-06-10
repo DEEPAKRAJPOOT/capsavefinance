b05e174131dbbbb34e073db6a15a8351115d85b3
pankaj.sharma <pankaj.sharma@prolitus.com>
2020-06-10 13:33:07 +0530

    application status issue

--- app/Http/Controllers/Application/ApplicationController.php
+++ app/Http/Controllers/Application/ApplicationController.php
@@ -446,7 +446,7 @@ class ApplicationController extends Controller
                 
             }
             
-            $wf_status = 1;                
+            $wf_status = 2;                
             Helpers::updateWfStage('doc_upload', $appId, $wf_status);
             
             $document_info = $this->docRepo->saveDocument($arrFileData, $docId, $userId);
