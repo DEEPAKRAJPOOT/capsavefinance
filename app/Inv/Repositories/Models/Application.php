1ea646f3a58b6b3b1ea13d283267f7864853419a
vishnu.kumar <vishnu.kumar@prolitus.com>
2020-06-16 16:34:28 +0530

    1234567890

--- app/Inv/Repositories/Models/Application.php
+++ app/Inv/Repositories/Models/Application.php
@@ -143,7 +143,7 @@ class Application extends BaseModel
                 'users.email',
                 'users.mobile_no',                
                 'app_assign.sharing_comment', 'assignee_r.name as assignee_role', 'from_r.name as from_role',
-                'app_assign.app_assign_id', 'app.parent_app_id')
+                'app_assign.app_assign_id', 'app.parent_app_id', 'note.note_data as reason')
                 ->join('users', 'users.user_id', '=', 'app.user_id')  
                 ->join('biz', 'app.biz_id', '=', 'biz.biz_id')
                 ->leftjoin('note','app.app_id', '=', 'note.app_id');
