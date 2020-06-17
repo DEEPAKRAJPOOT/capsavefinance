83e72c7e2e76311db555510f9bd62e6ebf5fea54
akash <akash.kumar@prolitus.com>
2020-06-17 15:15:42 +0530

    commented bank invoice

--- resources/views/layouts/backend/partials/admin-sidebar.blade.php
+++ resources/views/layouts/backend/partials/admin-sidebar.blade.php
@@ -234,9 +234,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{route('backend_get_invoice')}}">Manage Invoice</a>
                     </li>
-                    <li class="nav-item">
+                    <!-- <li class="nav-item">
                         <a class="nav-link" href="{{route('backend_get_bank_invoice')}}">Bank Invoice</a>
-                    </li>
+                    </li> -->
                     @endcan
                 </ul>
             </div>
