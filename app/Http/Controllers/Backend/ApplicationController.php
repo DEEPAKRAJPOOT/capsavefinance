5e7b4da43fb0f81f04dd0bfded0331ca797797d9
pankaj.sharma <pankaj.sharma@prolitus.com>
2020-06-29 15:10:23 +0530

    fixed #4423

--- app/Http/Controllers/Backend/ApplicationController.php
+++ app/Http/Controllers/Backend/ApplicationController.php
@@ -810,7 +810,7 @@ class ApplicationController extends Controller
                                
 				//Validate the stage
 				if ($currStage->stage_code == 'credit_mgr') {
-					$whereCondition = ['app_id' => $app_id, 'satus' => null];                                        
+					$whereCondition = ['app_id' => $app_id, 'status' => null];                                        
 					$offerData = $this->appRepo->getOfferData($whereCondition);
 					if (!$offerData) {
 						Session::flash('error_code', 'no_offer_found');
@@ -828,7 +828,7 @@ class ApplicationController extends Controller
                                             }                                            
                                         }
 				} else if ($currStage->stage_code == 'approver') {
-					$whereCondition = ['app_id' => $app_id, 'satus' => null];
+					$whereCondition = ['app_id' => $app_id, 'status' => null];
 					$offerData = $this->appRepo->getOfferData($whereCondition);
 					if (!$offerData) {
 						Session::flash('error_code', 'no_offer_found');
@@ -845,7 +845,7 @@ class ApplicationController extends Controller
 						}
 					}
 				} else if ($currStage->stage_code == 'sales_queue') {
-					$whereCondition = ['app_id' => $app_id];
+					$whereCondition = ['app_id' => $app_id, 'status' => null];
 					$offerData = $this->appRepo->getOfferData($whereCondition);
 					if (isset($offerData->status) && empty($offerData->status) ) {
 						Session::flash('error_code', 'no_offer_accepted');
