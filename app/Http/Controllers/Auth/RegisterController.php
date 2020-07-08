be6959936747add4171542db9fda78d58102d1f7
vikrant kumar <vikrant.kumar@prolitus.com>
2020-07-08 16:48:26 +0530

    #4402

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -631,7 +631,7 @@ use RegistersUsers,
 
         if (!(Hash::check($request->get('current-password'), Auth::user()->password))) {
             // The passwords matches
-            return redirect()->back()->with("error", "Your current password does not matches with the password you provided. Please try again.");
+            return redirect()->back()->with("error", "Your old password does not matches with the password you provided. Please try again.");
         }
         if (strcmp($request->get('current-password'), $request->get('new-password')) == 0) {
             //Current password and new password are same
