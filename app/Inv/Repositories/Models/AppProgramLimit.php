33c9750507859bc4f998f0284f9c58aa8bb20be6
Ravi prakash <ravi.awasthi@prolitus.com>
2020-07-08 16:06:36 +0530

    Program Limit Issue while creating offer and change in InvoiceDate according to EOD Date

--- app/Inv/Repositories/Models/AppProgramLimit.php
+++ app/Inv/Repositories/Models/AppProgramLimit.php
@@ -240,7 +240,7 @@ class AppProgramLimit extends BaseModel {
             throw new InvalidDataTypeExceptions(trans('error_message.invalid_data_type'));
         }
 
-        return AppProgramOffer::where('prgm_id', $program_id)->sum('prgm_limit_amt');
+        return AppProgramOffer::where('prgm_id', $program_id)->where('is_active', '1')->sum('prgm_limit_amt');
      }
 
     public function appLimit(){
