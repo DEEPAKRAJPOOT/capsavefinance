3fea1b2c299f192529705f8b07a0630e4247ce9b
vishnu.kumar <vishnu.kumar@prolitus.com>
2020-07-09 10:54:27 +0530

    1234567890

--- resources/views/backend/app/offer.blade.php
+++ resources/views/backend/app/offer.blade.php
@@ -255,7 +255,7 @@ tr.border_bottom td {
                                         <table cellspacing="0" cellpadding="0" width="100%" class="table table-striped table-bordered">
                                             <thead>
                                                 <tr role="row" style="background: #62b59b;color: #fff; text-align: center;">
-                                                   <th width="5%">Sr. No.</th>
+                                                   <th width="5%">Offer No.</th>
                                                    <th width="80%" colspan="4">Offer Details</th>
                                                    <th width="15%">Status</th>
                                                 </tr>
@@ -344,7 +344,7 @@ tr.border_bottom td {
                                         <table cellspacing="0" cellpadding="0" width="100%" class="table table-striped table-bordered">
                                             <thead>
                                                 <tr role="row" style="background: #62b59b;color: #fff; text-align: center;">
-                                                   <th width="5%">Sr. No.</th>
+                                                   <th width="5%">Offer No.</th>
                                                    <th width="80%" colspan="4">Offer Details</th>
                                                    <th width="15%">Status</th>
                                                 </tr>
