308523e4613e141a07fe604121c15e7b38ed19a7
hyphen <hyphen.call@gmail.com>
2020-07-13 17:52:56 +0530

    ss

--- resources/views/backend/lead/index.blade.php
+++ resources/views/backend/lead/index.blade.php
@@ -69,7 +69,7 @@
                                     <table id="leadMaster" class="table white-space table-striped cell-border dataTable no-footer overview-table" cellspacing="0" width="100%" role="grid" aria-describedby="supplier-listing_info" style="width: 100%;">
                                         <thead>
                                             <tr role="row">
-                                                <th>Sr.No.</th>
+                                                <th>Lead.ID.</th>
                                                 <th>Name</th>
                                                 <th>Email</th>
                                                 <th>Mobile</th>
