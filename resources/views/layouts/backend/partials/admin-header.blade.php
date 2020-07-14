4961fa5ef0c9f324224528b9c74ef0dab37ec455
vikrant kumar <vikrant.kumar@prolitus.com>
2020-07-14 15:53:07 +0530

    #4557

--- resources/views/layouts/backend/partials/admin-header.blade.php
+++ resources/views/layouts/backend/partials/admin-header.blade.php
@@ -13,8 +13,8 @@
         </form>-->
         <ul class="navbar-nav ml-lg-auto relative">
             <li class="nav-item nav-profile">
-                <span style="color: #328964; position: absolute; right: 28px; min-width: 350px; top: 3px;">Current System Time : {{Helpers::getSysCurrentDate()}}</span>
-                <a class="nav-link" href="#">
+                <span style="color: #328964; position: absolute; right: 28px; min-width: 350px; top: 3px;"><b>Current System Time:</b> {{Helpers::getSysCurrentDate()}}</span>
+                <a class="nav-link" href="#" style="margin-top: 15px;">
                     <span style="color: #328964;">{{ucwords(Auth::user()->f_name.' '.Auth::user()->l_name)}}</span>
                     <img src="{{url('backend/assets/images/faces/face9.jpg')}}" />
                 </a>
