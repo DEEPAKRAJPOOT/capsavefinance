31b5168fc6c3c701e88a0e21d6fb8d4517cbf6ab
pankaj.sharma <pankaj.sharma@prolitus.com>
2020-07-16 13:06:50 +0530

    #4544

--- app/Http/Controllers/Backend/CamController.php
+++ app/Http/Controllers/Backend/CamController.php
@@ -1559,7 +1559,7 @@ class CamController extends Controller
                 Session::flash('error', trans('backend_messages.enhanced_tot_limit_amt_validation'));
                 return redirect()->back()->withInput();                
             } else if ($result['app_type'] == 3 && isset($result['tot_limit_amt']) 
-                    && $result['tot_limit_amt'] < $totLimitAmt && $result['parent_inv_utilized_amt'] >= $totLimitAmt) {
+                    && ($result['tot_limit_amt'] < $totLimitAmt || $result['parent_inv_utilized_amt'] >= $totLimitAmt)) {
                 Session::flash('error', trans('backend_messages.reduced_tot_limit_amt_validation'));
                 return redirect()->back()->withInput();                
             } else if ($result['status']) {
