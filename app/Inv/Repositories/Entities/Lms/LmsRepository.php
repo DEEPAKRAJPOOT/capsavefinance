2575c596a839dd8013104f75cfc233c01216e82e
hyphen <hyphen.call@gmail.com>
2020-07-22 15:07:20 +0530

    ss

--- app/Inv/Repositories/Entities/Lms/LmsRepository.php
+++ app/Inv/Repositories/Entities/Lms/LmsRepository.php
@@ -1381,8 +1381,18 @@ class LmsRepository extends BaseRepositories implements LmsInterface {
 
 	public function getColenderApplications() 
        {
+            $roleData = User::getBackendUser(\Auth::user()->user_id);
             $getAppId  = ColenderShare::where(['is_active' => 1, 'co_lender_id' => \Auth::user()->co_lender_id])->pluck('app_id');
-            $result = LmsUser::whereIn('app_id',$getAppId)->with('user')->orderBy('lms_user_id','DESC');
+			$result = LmsUser::whereIn('app_id',$getAppId)->with('user')->orderBy('lms_user_id','DESC');
+
+			 if ($roleData[0]->is_superadmin != 1) {
+				$getAppId  = ColenderShare::where(['is_active' => 1, 'co_lender_id' => \Auth::user()->co_lender_id])->pluck('app_id');
+				$result = LmsUser::whereIn('app_id',$getAppId)->with('user')->orderBy('lms_user_id','DESC');
+			} else {
+				$getAppId  = ColenderShare::where(['is_active' => 1])->pluck('app_id');
+				$result = LmsUser::whereIn('app_id',$getAppId)->with('user')->orderBy('lms_user_id','DESC');
+			}
+			
             return $result ?: false;
 	}
         
