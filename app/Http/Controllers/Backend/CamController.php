c46a1f7a714ffcea661dd1558c87abd5ed91c2e7
Ravi prakash <ravi.awasthi@prolitus.com>
2020-07-23 10:16:57 +0530

    need to delete

--- app/Http/Controllers/Backend/CamController.php
+++ app/Http/Controllers/Backend/CamController.php
@@ -631,12 +631,9 @@ class CamController extends Controller
         $fy = $contents['FinancialStatement']['FY'] ?? array();
         $financeData = [];
         $audited_years = [];
-        $curr_fin_year = ((date('m') > 3) ? date('Y') : (date('Y') - 1));
+        $curr_fin_year = date('Y') - 1;
         if (!empty($fy)) {
           foreach ($fy as $k => $v) {
-            if (!empty($v['year']) && $k == 0) {
-              $curr_fin_year = $v['year'];
-            }
             if ($this->genBlankfinJSON) {
               $v['year'] = empty($v['year']) ? $curr_fin_year : $v['year'];
               $curr_fin_year--;
