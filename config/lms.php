c52d2889884270258579e5a3b23a86d3b418a999
akash <akash.kumar@prolitus.com>
2020-12-07 17:52:59 +0530

    account details change

--- config/lms.php
+++ config/lms.php
@@ -203,7 +203,7 @@ return [
     ],
     'IDFC_DEBIT_BANK' => [    
        'DEBIT_ACC_NO' => '10062193074',
-       'DEBIT_ACC_NAME' => '',
+       'DEBIT_ACC_NAME' => 'Capsave Finance Pvt Ltd',
        'DEBIT_MOBILE' => '9930840248
 
 '
