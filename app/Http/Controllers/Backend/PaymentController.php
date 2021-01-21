b9fd63434d67849cc8d20f43dcb7cee82ad7ff90
amit.suman <amit.suman@prolitus.com>
2021-01-21 18:18:48 +0530

    re #cahnges

--- app/Http/Controllers/Backend/PaymentController.php
+++ app/Http/Controllers/Backend/PaymentController.php
@@ -795,7 +795,7 @@ class PaymentController extends Controller {
             //echo $fullFilePath; exit;
 
             $header = [
-                0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28
+                0,1,2,3,4,5,6,7,8,9,10,11,12,13
             ];
             $fileArrayData = $this->fileHelper->excelNcsv_to_array($fullFilePath, $header);
 
