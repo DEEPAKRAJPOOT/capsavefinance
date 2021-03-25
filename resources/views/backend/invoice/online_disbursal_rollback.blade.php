2a896f7f563ab54ba33adaf5abb76c9e322cc92d
vishnu.kumar <vishnu.kumar@prolitus.com>
2021-03-25 17:28:32 +0530

    1234567890

--- resources/views/backend/invoice/online_disbursal_rollback.blade.php
+++ resources/views/backend/invoice/online_disbursal_rollback.blade.php
@@ -26,7 +26,9 @@
             @if($res_text)
             <tr>
                 <td><b>IDFC API Response:</b></td>
-                <td colspan="3" style="width: 100px">{{$res_text}}</td>
+                <td colspan="3">
+                    <div style="width:500px;white-space:initial;">{{$res_text}}</div>
+                </td>
             </tr>
             @endif
         </tbody>
