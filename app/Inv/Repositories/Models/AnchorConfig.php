de1c2319067ffec8b5eb5f59113ff984fd8c93a6
vishnu.kumar <vishnu.kumar@prolitus.com>
2021-05-18 20:11:33 +0530

    1234567890

--- app/Inv/Repositories/Models/AnchorConfig.php
+++ app/Inv/Repositories/Models/AnchorConfig.php
@@ -4,7 +4,39 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class AnchorConfig.php extends Model
+class AnchorConfig extends Model
 {
-    //
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'anchor_config';
+
+    /**
+     * Custom primary key is set for the table
+     *
+     * @var integer
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Maintain created_at and updated_at automatically
+     *
+     * @var boolean
+     */
+    public $timestamps = true;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'anchor_id',
+        'css_file',
+        'redirect_url',
+        'created_at',
+        'updated_at'
+    ];
 }
