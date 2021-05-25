97b30a00cf57c86d4e193b956e7bbb38815c09d1
sudesh.kumar <sudesh.kumar@prolitus.com>
2021-05-25 21:10:11 +0530

    fixed

--- app/Helpers/ManualApportionmentHelperTemp.php
+++ app/Helpers/ManualApportionmentHelperTemp.php
@@ -268,7 +268,7 @@ class ManualApportionmentHelperTemp{
                 InterestAccrualTemp::where('payment_id',$paymentId)->delete();
                 $paymentDate = $payment->date_of_payment;
                 $userId = $payment->user_id;
-                $invoiceList = InvoiceDisbursed::whereNotNull('int_accrual_start_dt') ->whereNotNull('payment_due_date') ->where('user_id',$userId) ->pluck('invoice_disbursed_id','invoice_disbursed_id');
+                $invoiceList = InvoiceDisbursed::whereNotNull('int_accrual_start_dt') ->whereNotNull('payment_due_date') ->whereHas('invoice', function($query){ $query->where('supplier_id',$userId); }) ->pluck('invoice_disbursed_id','invoice_disbursed_id');
                 foreach ($invoiceList as $invId => $trans) {
                     $this->intAccrual($invId, null, $paymentDate, $paymentId);
                 }
