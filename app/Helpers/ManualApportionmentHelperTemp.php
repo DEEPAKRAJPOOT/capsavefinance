d54ffd3583a195dc561a18d7ac719a136f6e8d9d
sudesh.kumar <sudesh.kumar@prolitus.com>
2021-05-25 20:48:45 +0530

    fixed issue

--- app/Helpers/ManualApportionmentHelperTemp.php
+++ app/Helpers/ManualApportionmentHelperTemp.php
@@ -269,7 +269,7 @@ class ManualApportionmentHelperTemp{
                 $paymentDate = $payment->date_of_payment;
                 $userId = $payment->user_id;
                 $invoiceList = $this->lmsRepo->getUnsettledInvoices(['noNPAUser'=>true, 'intAccrualStartDateLteSysDate'=>true, 'user_id'=>$userId]);
-                foreach ($arrayInvoiceSlice as $invId => $trans) {
+                foreach ($invoiceList as $invId => $trans) {
                     $this->intAccrual($invId, null, $paymentDate, $paymentId);
                 }
             }
