4bbb2847cf1d243f5a8f2dc2ee7e5bec43383171
vikrant kumar <vikrant.kumar@prolitus.com>
2021-06-16 16:30:15 +0530

    #334

--- app/Inv/Repositories/Events/UserEventsListener.php
+++ app/Inv/Repositories/Events/UserEventsListener.php
@@ -1034,13 +1034,8 @@ class UserEventsListener extends BaseEvent
             Mail::send('email', ['baseUrl'=>env('REDIRECT_URL',''),'varContent' => $mail_body,
                 ],
                 function ($message) use ($user, $email_content, $mail_body) {
-                    if( env('SEND_MAIL_ACTIVE') == 1){
-                        $email = explode(',', env('SEND_MAIL'));
-                        $message->cc(explode(',', config('common.LENEVO_SEND_MAIL_CC')));
-                    }else{
-                        $email = $user["email"];
-                        $message->cc(explode(',', config('common.LENEVO_SEND_MAIL_CC')));
-                    }
+                $email = $user["email"];
+                $message->cc(explode(',', config('common.LENEVO_SEND_MAIL_CC')));                    
                 $message->from(config('common.FRONTEND_FROM_EMAIL'), config('common.FRONTEND_FROM_EMAIL_NAME'));
                 $message->to($email, $user["name"])->subject($email_content->subject);
                 $mailContent = [
