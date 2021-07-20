ad028f140c45e7ee1596e7c28f1fdcac315908b4
sudesh.kumar <sudesh.kumar@zuron.in>
2021-07-20 19:54:23 +0530

    fixed issue

--- app/Inv/Repositories/Entities/Lms/UserInvoiceRepository.php
+++ app/Inv/Repositories/Entities/Lms/UserInvoiceRepository.php
@@ -105,11 +105,16 @@ class UserInvoiceRepository extends BaseRepositories implements UserInvoiceInter
 		$UserInvoiceTxns = Transactions::getUserInvoiceTxns($userId, $invoiceType, $transIds, $is_force);
 		if (!empty($UserInvoiceTxns)) {
 			foreach ($UserInvoiceTxns as $key => $txn) {
-				$waiveOffAmount = $txn->getWaiveOffAmount();
-				$txn->amount = $txn->amount -$waiveOffAmount;
-				if ($txn->amount == 0) {
-					unset($UserInvoiceTxns[$key]);
+				if($txn->trans_type = config('lms.TRANS_TYPE.INVOICE_PROCESSING_FEE')){
+					$txn->amount = $txn->amount;
+				}else{
+					$txn->amount = $txn->Outstanding;
 				}
+				// $waiveOffAmount = $txn->getWaiveOffAmount();
+				// $txn->amount = $txn->amount - $waiveOffAmount;
+				// if ($txn->amount == 0) {
+				// 	unset($UserInvoiceTxns[$key]);
+				// }
 			}
 		}
 		return $UserInvoiceTxns;
