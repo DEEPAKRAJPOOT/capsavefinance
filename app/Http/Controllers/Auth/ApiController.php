3fb2c852236c09d093342ab7b6ae93ebc7cb63ae
sudesh.kumar <sudesh.kumar@zuron.in>
2022-02-02 13:46:35 +0530

    fixed issue

--- app/Http/Controllers/Auth/ApiController.php
+++ app/Http/Controllers/Auth/ApiController.php
@@ -649,24 +649,24 @@ class ApiController
 
   public function tally_entry_Week_wise($weekName){
     $activeDate = date('Y-m-d');
-    //$dates = $this->displayDates('2020-01-01', date('Y-m-d'));
-    //foreach ($dates as $activeDate) {
+    $dates = $this->displayDates('2022-01-01', date('Y-m-d'));
+    foreach ($dates as $activeDate) {
       if(in_array(strtolower(trim($weekName)),[strtolower(date('D',strtotime($activeDate))), strtolower(date('l',strtotime($activeDate)))])){
         $weekStartDate = date('Y-m-d',(strtotime ( '-7 day' , strtotime($activeDate))));
         self::tally_entry($weekStartDate,$activeDate);
       }
-    //}
+    }
   }
 
   public function tally_entry_month_wise(){
     $activeDate = date('Y-m-d');
-    //$dates = $this->displayDates('2020-01-01', date('Y-m-d'));
-    //foreach ($dates as $activeDate) {
+    $dates = $this->displayDates('2020-01-01', '2021-12-31');
+    foreach ($dates as $activeDate) {
       if(date("Y-m-t", strtotime($activeDate)) == $activeDate){
         $monthStartDate = date("Y-m-1", strtotime($activeDate));
         self::tally_entry($monthStartDate,$activeDate);
       }
-    //}
+    }
   }
 
   public function tally_entry($startDate = null, $endDate = null){
