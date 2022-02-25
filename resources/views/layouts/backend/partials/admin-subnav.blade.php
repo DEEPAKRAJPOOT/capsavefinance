4fd8aeeb1b1f904e22627657ec9f312f99aaecb6
Deepak Jha <deepak.jha@zuron.in>
2022-02-25 23:17:35 +0530

    changes letter

--- resources/views/layouts/backend/partials/admin-subnav.blade.php
+++ resources/views/layouts/backend/partials/admin-subnav.blade.php
@@ -69,8 +69,6 @@
     </li>
     @endcan
     @php
-        $appSanctionLetterDataFlag = \Helpers::appSanctionLetterStatus(request()->get('app_id'));  
-        $appCurrentStatus = \Helpers::appCurrentStatus(request()->get('app_id'));
         $appData = \Helpers::appDataCurrent(request()->get('app_id')); 
         $productsArr = $appData->products->pluck('id')->toArray();
         $appSanctionLetterGenerated = \Helpers::appSanctionLetterGenerated(request()->get('app_id'));      
@@ -81,7 +79,7 @@
         <a href="{{ route('gen_sanction_letter', ['app_id' => request()->get('app_id'), 'biz_id' => request()->get('biz_id')]) }}" class="{{ request()->is('application/sanction-letter') ? 'active' : '' }}">Sanction Letter</a>
     </li>
     @endcan 
-    @if(!$appSanctionLetterDataFlag && !$appSanctionLetterGenerated)
+    @if(!$appSanctionLetterGenerated)
     @if (in_array(1, $productsArr))
     @can('list_new_sanction_letter')
     <li>
