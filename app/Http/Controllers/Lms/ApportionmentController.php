2ed1e2ca772112c9b9edf844131f2dbb69fc3905
Sudesh <sudesh.kumar@zuron.in>
2022-03-10 17:32:50 +0530

    fixed issue

--- app/Http/Controllers/Lms/ApportionmentController.php
+++ app/Http/Controllers/Lms/ApportionmentController.php
@@ -1441,8 +1441,8 @@ class ApportionmentController extends Controller
                 foreach ($payments as $transDate => $payment) {
                     $paymentData = [
                         'user_id' => $transactions[0]->user_id,
-                        'biz_id' => $transactions[0]->payment->biz_id,
-                        'virtual_acc' => $transactions[0]->payment->virtual_acc,
+                        'biz_id' => $transactions[0]->payment->biz_id ?? NULL,
+                        'virtual_acc' => $transactions[0]->payment->virtual_acc ?? NULL,
                         'action_type' => 5,
                         'trans_type' => config('lms.TRANS_TYPE.ADJUSTMENT'),
                         'amount' => $payment['amount'],
