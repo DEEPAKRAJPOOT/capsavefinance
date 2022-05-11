5ce5c58ded7d7ab8f59cf8ee5282821b67ad391b
Sudesh <sudesh.kumar@zuron.in>
2022-05-11 19:07:02 +0530

    fixed

--- app/Libraries/Ui/DataRenderer.php
+++ app/Libraries/Ui/DataRenderer.php
@@ -6858,11 +6858,11 @@ class DataRenderer implements DataProviderInterface
            ->editColumn('interest_prd',  function ($invoiceRec) {
              $txn = Transactions::find($invoiceRec->transId);
              $desc = $txn->transType->trans_name ?? NULL;
-            if ($txn->transTypeId == config('lms.TRANS_TYPE.INTEREST')) {
+            if ($invoiceRec->transTypeId == config('lms.TRANS_TYPE.INTEREST')) {
                 $desc =  "Interest for period " . date('d-M-Y', strtotime($txn->fromIntDate)) . " To " . date('d-M-Y', strtotime($txn->toIntDate));
             } 
 
-            if ($txn->transTypeId == config('lms.TRANS_TYPE.INTEREST_OVERDUE')) {
+            if ($invoiceRec->transTypeId == config('lms.TRANS_TYPE.INTEREST_OVERDUE')) {
                 $dueDate = strtotime($txn->toIntDate); // or your date as well
                 $now = strtotime($txn->fromIntDate);
                 $datediff = ($dueDate - $now);
