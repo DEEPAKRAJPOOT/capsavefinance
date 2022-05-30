081c61d480a92c8b0524b85c55efe9d9dfb02be6
sarthak <sarthak.kaushish@zuron.in>
2022-05-30 18:29:13 +0530

    Font Changes

--- app/Inv/Repositories/Events/UserEventsListener.php
+++ app/Inv/Repositories/Events/UserEventsListener.php
@@ -1500,24 +1500,24 @@ class UserEventsListener extends BaseEvent
                $requestPara = $val['app_id'].'%'.$val['biz_id'];
             $rowData .='<tr>
               <td
-                style="font-family: Calibri !important; box-sizing: border-box; font-size: 0.917rem !important; text-align: left; padding: 10px 10px 10px 0px; border-top:1px solid #ccc;border-right:1px solid #ccc;padding: 2px 5px;font-size: 0.917rem !important;line-height: 18px;vertical-align: top;width: 10%;">
+                style="font-family: Calibri !important; box-sizing: border-box; font-size: 10px !important; text-align: left; padding: 10px 10px 10px 0px; border-top:1px solid #ccc;border-right:1px solid #ccc;padding: 2px 5px;font-size: 10px !important;line-height: 18px;vertical-align: top;width: 10%;">
                 '.$val['app_code'].'
               </td>
               <td
-                style="font-family: Calibri !important; box-sizing: border-box; font-size: 0.917rem !important; text-align: left; padding: 10px 10px 10px 0px; border-top:1px solid #ccc;border-right:1px solid #ccc;padding: 2px 5px;font-size: 0.917rem !important;line-height: 18px;vertical-align: top; width: 20%;">
+                style="font-family: Calibri !important; box-sizing: border-box; font-size: 10px !important; text-align: left; padding: 10px 10px 10px 0px; border-top:1px solid #ccc;border-right:1px solid #ccc;padding: 2px 5px;font-size: 10px !important;line-height: 18px;vertical-align: top; width: 20%;">
                 '. $val['customer_name'].'
               </td>
               <td
-                style="font-family: Calibri !important; box-sizing: border-box; font-size: 0.917rem !important; text-align: left; padding: 10px 10px 10px 0px; border-top:1px solid #ccc;border-right:1px solid #ccc;padding: 2px 5px;font-size: 0.917rem !important;line-height: 18px;vertical-align: top; width: 50%;">
+                style="font-family: Calibri !important; box-sizing: border-box; font-size: 10px !important; text-align: left; padding: 10px 10px 10px 0px; border-top:1px solid #ccc;border-right:1px solid #ccc;padding: 2px 5px;font-size: 10px !important;line-height: 18px;vertical-align: top; width: 50%;">
                 '. $val['biz_entity_name'].'
               </td>
               <td
-                style="font-family: Calibri !important; box-sizing: border-box; font-size: 0.917rem !important; text-align: left; padding: 10px 10px 10px 0px; border-top:1px solid #ccc;border-right:1px solid #ccc;padding: 2px 5px;font-size: 0.917rem !important;line-height: 18px;vertical-align: top; width: 10%;">
+                style="font-family: Calibri !important; box-sizing: border-box; font-size: 10px !important; text-align: left; padding: 10px 10px 10px 0px; border-top:1px solid #ccc;border-right:1px solid #ccc;padding: 2px 5px;font-size: 10px !important;line-height: 18px;vertical-align: top; width: 10%;">
                 '.\Helpers::formatCurreny($val['prgm_limit_amt']).'
               </td>
               <td
-                style="font-family: Calibri !important; box-sizing: border-box; font-size: 0.917rem !important; text-align: left; padding: 10px 10px 10px 0px; border-top:1px solid #ccc;border-right:1px solid #ccc;padding: 2px 5px;font-size: 0.917rem !important;line-height: 18px;vertical-align: top; width: 10%;">
-                '. $int .'
+                style="font-family: Calibri !important; box-sizing: border-box; font-size: 10px !important; text-align: left; padding: 10px 10px 10px 0px; border-top:1px solid #ccc;border-right:1px solid #ccc;padding: 2px 5px;font-size: 10px !important;line-height: 18px;vertical-align: top; width: 10%;">
+                '. $int .' %
               </td>
             </tr>';
             }
