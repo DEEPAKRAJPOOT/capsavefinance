fb315ff1abdedc08aeb4faaffa9cc5f4b9b942ad
sarthak <sarthak.kaushish@zuron.in>
2022-05-31 10:57:42 +0530

    Amount column width change

--- app/Inv/Repositories/Events/UserEventsListener.php
+++ app/Inv/Repositories/Events/UserEventsListener.php
@@ -1512,11 +1512,11 @@ class UserEventsListener extends BaseEvent
                 '. $val['biz_entity_name'].'
               </td>
               <td
-                style="font-family: Calibri !important; box-sizing: border-box; font-size: 10px !important; text-align: left; padding: 10px 10px 10px 0px; border-top:1px solid #ccc;border-right:1px solid #ccc;padding: 2px 5px;font-size: 10px !important;line-height: 18px;vertical-align: top; width: 10%;">
+                style="font-family: Calibri !important; box-sizing: border-box; font-size: 10px !important; text-align: left; padding: 10px 10px 10px 0px; border-top:1px solid #ccc;border-right:1px solid #ccc;padding: 2px 5px;font-size: 10px !important;line-height: 18px;vertical-align: top; width: 15%;">
                 '.\Helpers::formatCurreny($val['prgm_limit_amt']).'
               </td>
               <td
-                style="font-family: Calibri !important; box-sizing: border-box; font-size: 10px !important; text-align: left; padding: 10px 10px 10px 0px; border-top:1px solid #ccc;border-right:1px solid #ccc;padding: 2px 5px;font-size: 10px !important;line-height: 18px;vertical-align: top; width: 10%;">
+                style="font-family: Calibri !important; box-sizing: border-box; font-size: 10px !important; text-align: left; padding: 10px 10px 10px 0px; border-top:1px solid #ccc;border-right:1px solid #ccc;padding: 2px 5px;font-size: 10px !important;line-height: 18px;vertical-align: top; width: 5%;">
                 '. $int .' %
               </td>
             </tr>';
