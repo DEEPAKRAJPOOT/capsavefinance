124c1817289cda964b15c9f99597b01ce88962ba
amit.suman <amit.suman@prolitus.com>
2022-06-22 18:18:13 +0530

    r e#cahgens

--- app/Inv/Repositories/Events/UserEventsListener.php
+++ app/Inv/Repositories/Events/UserEventsListener.php
@@ -848,7 +848,7 @@ class UserEventsListener extends BaseEvent
 
     public function onDisbursedSuccess($userData) {
 
-        
+
         $user = unserialize($userData);
         $this->func_name = __FUNCTION__;
         //Send mail to User
@@ -869,18 +869,17 @@ class UserEventsListener extends BaseEvent
             }
             if(isset($user['auth_email'])) {
                 $email_cc[] = ($user['auth_email']);
-            } 
+            }
             Mail::send('email', ['baseUrl'=>env('REDIRECT_URL',''),'varContent' => $mail_body, ],
                 function ($message) use ($user, $mail_subject, $mail_body, $email_cc) {
                 $message->from(config('common.FRONTEND_FROM_EMAIL'), config('common.FRONTEND_FROM_EMAIL_NAME'));
-                $message->to(trim('gaurav.agarwal@zuron.in'),trim($user["receiver_user_name"]))->subject($mail_subject);
+                $message->to(trim($user["receiver_email"]),trim($user["receiver_user_name"]))->subject($mail_subject);
                 if(!empty($email_cc)) {
                     $message->cc($email_cc);
                 }
                 $mailContent = [
                     'email_from' => config('common.FRONTEND_FROM_EMAIL'),
-                    //'email_to' => array($user["receiver_email"]),
-                    'email_to' => 'gaurav.agarwal@zuron.in',
+                    'email_to' => array($user["receiver_email"]),
                     'email_cc' => $email_cc,
                     'email_type' => $this->func_name,
                     'name' => $user['receiver_user_name'],
@@ -890,7 +889,7 @@ class UserEventsListener extends BaseEvent
                 FinanceModel::logEmail($mailContent);
             });
         }
-    } 
+    }
 
     public function onMaturityReport($mailData){
         $data = unserialize($mailData); 
