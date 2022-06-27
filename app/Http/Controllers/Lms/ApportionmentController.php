260bc306e718730cda2456e94338ebe43429afd7
Sudesh <sudesh.kumar@zuron.in>
2022-06-27 20:07:26 +0530

    fixed issue

--- app/Http/Controllers/Lms/ApportionmentController.php
+++ app/Http/Controllers/Lms/ApportionmentController.php
@@ -1822,6 +1822,11 @@ class ApportionmentController extends Controller
                 return redirect()->route('unsettled_payments')->withErrors('Someone is already trying to settle transactions')->withInput();
             }
 
+            if ($payment && $payment->is_settled == Payment::PAYMENT_SETTLED_PENDING) {
+                DB::rollback();
+                return redirect()->route('unsettled_payments')->withErrors('Unable to perform Apportionment, Please retry!')->withInput();
+            }
+            
             if ($payment && $payment->is_settled == Payment::PAYMENT_SETTLED_PROCESSED) {
                 DB::rollback();
                 return redirect()->route('unsettled_payments')->withErrors('Someone is already trying to settle transactions')->withInput();
