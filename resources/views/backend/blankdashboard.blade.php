b7890924d823d0ef759ffafda4eeb8f76eb2e3ba
Sudesh <sudesh.kumar@zuron.in>
2022-08-04 17:05:50 +0530

    fixed issue

--- resources/views/backend/blankdashboard.blade.php
+++ resources/views/backend/blankdashboard.blade.php
@@ -6,9 +6,10 @@
 		
 		<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-4">
 			<div class="card card-statistics">
-				<div class="card-body m-0" style=" height: 500px!important;">
+				<div class="card-body m-0" style=" height: 500px!important; ">
 						<div class="float-left">
-							<h4 class="bold-text">The Dashboard section is currently undergoing maintenance.</h4>
+							<h4 class="bold-text">Dashboard</h4>
+                            <h6 class="bold-text">The Dashboard section is currently undergoing maintenance.</h6>
 						</div>
 					</div>
 				</div>
