3fd7199b246150522b07fb78351287bf7d93cd60
deepak <deepak.singh@zeeve.io>
2022-09-28 10:21:39 +0530

    change on tally batch for request

--- app/Http/Controllers/Auth/ApiController.php
+++ app/Http/Controllers/Auth/ApiController.php
@@ -680,12 +680,13 @@ class ApiController
     // }
   }
 
-  public function tally_entry($startDate = null, $endDate = null){
+  public function tally_entry($startDate = null, $endDate = null, Request $request){
+    
     if(empty($startDate)){
-      $startDate = date('Y-m-d');
+      $startDate = $request->get('start_date')?date('Y-m-d',strtotime($request->get('start_date'))):date('Y-m-d');
     }
     if(empty($endDate)){
-      $endDate = $startDate;
+      $endDate = $request->get('end_date')?date('Y-m-d',strtotime($request->get('end_date'))):$startDate;
     }
     $this->selectedTxnData = [];
     $this->selectedPaymentData = [];
