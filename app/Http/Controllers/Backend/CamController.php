c1ecb4056767095ae2937211fb2ec57c49055bdb
sarthak <sarthak.kaushish@zuron.in>
2022-12-16 14:59:03 +0530

    Change In Error msg

--- app/Http/Controllers/Backend/CamController.php
+++ app/Http/Controllers/Backend/CamController.php
@@ -2025,7 +2025,7 @@ class CamController extends Controller
           if ($prgm_data && $prgm_data->product_id == 1) {
             $offerIsExist = \Helpers::checkAnchorPrgmOfferDuplicate($prgm_data->anchor_id, $program_id, $appId);
           if ((!$prgmOfferId && $offerIsExist) || (!$prgmOfferId && !$offerIsExist) || ($prgmOfferId && $offerIsExist && $prgmOfferId != $offerIsExist->prgm_offer_id)) {
-            Session::flash('error', 'Anchor Offer is already generated for this program.');
+            Session::flash('error', 'Application offer already generated for this Program & Anchor.');
             return redirect()->route('limit_assessment',['app_id' =>  $appId, 'biz_id' => $bizId]);
           }
 
