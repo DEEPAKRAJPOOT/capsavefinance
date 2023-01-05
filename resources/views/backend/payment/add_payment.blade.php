655c9d662c19266612a067781fa83507eaa0c84e
sarthak <sarthak.kaushish@zuron.in>
2023-01-05 17:52:38 +0530

    fixed

--- resources/views/backend/payment/add_payment.blade.php
+++ resources/views/backend/payment/add_payment.blade.php
@@ -615,7 +615,8 @@ $.validator.addMethod('decimal', function(value, element) {
         }, "Please enter only 2 decimal places");
         $("#amount").keyup(function(){
             $(this).val(function(i, v) {
-    return v.replace(/,/g, '');
+    return v.replace(/[, ]+/g, "");
+    
   });
         });
         $('#savePayFrm').validate( {
