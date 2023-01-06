6bf9a2fe11b9eea90b55315c02cf190b524f93ae
Sudesh <sudesh.kumar@zuron.in>
2023-01-06 18:47:28 +0530

    fixed issue

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -77,7 +77,7 @@ class Kernel extends ConsoleKernel
             $schedule->command('report:receipt')->timezone(config('common.timezone'))->dailyAt('23:52');
             // To Generate Outstanding Report Manual
             $schedule->command('report:reconReport')->timezone(config('common.timezone'))->dailyAt('23:54');
-            $schedule->command('report:outstandingManual')->timezone(config('common.timezone'))->dailyAt('18:47');
+            $schedule->command('report:outstandingManual')->timezone(config('common.timezone'))->dailyAt('18:48');
         }
         $schedule->command('command:lenovoNewUser')->timezone(config('common.timezone'))->dailyAt('23:00');
         $schedule->command('lms:maturityinvoicedueAlert')->timezone(config('common.timezone'))->dailyAt('21:30');
