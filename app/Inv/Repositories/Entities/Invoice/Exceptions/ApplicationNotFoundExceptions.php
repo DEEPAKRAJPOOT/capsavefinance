d5be14c57a7abe7876f427b7cd7b29d6a737a3b4
sudesh <sudesh.kumar@zuron.in>
2023-04-01 06:43:45 +0530

    Upgrade Laravel 10 Major Changes

--- app/Inv/Repositories/Entities/Invoice/Exceptions/ApplicationNotFoundExceptions.php
+++
@@ -1,7 +0,0 @@
-<?php namespace App\Inv\Repositories\Entities\Application\Exceptions;
-
-use App\Inv\Repositories\Entities\Application\Exceptions\ApplicationExceptions;
-
-class ApplicationNotFoundExceptions extends ApplicationExceptions
-{
-}
