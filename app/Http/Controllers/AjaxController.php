447b61b42c7e9d6918536d8ef5095ad819f09afc
sarthak <sarthak.kaushish@zuron.in>
2023-04-03 13:12:06 +0530

    fixedIssue

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -6427,8 +6427,8 @@ if ($err) {
      }
 
      public function getCustAndCapsLocApp(DataProviderInterface $dataProvider) {
-        // dd($this->request->all(),$this->request->get('user_id'));
-        $user_id =  (int) $this->request->get('user_id');
+        $appData = $this->application->getAppDataByAppId($this->request->get('app_id'));
+        $user_id = $appData->user_id;
         $cusCapLoc = $this->UserInvRepo->getCustAndCapsLocApp($user_id);
         $data = $dataProvider->getCustAndCapsLocApp($this->request, $cusCapLoc);
         return $data;
