594d4479f79dc1bc3c678347b8aa1a7cab9f698c
Deepak Jha <deepak.jha@zuron.in>
2023-05-10 10:44:19 +0530

    add feature to caclate on the page when upload invoice

--- public/backend/js/single_invoice.js
+++ public/backend/js/single_invoice.js
@@ -586,7 +586,7 @@
     var invoice_approve_amount = parseInt(invoice_approve_amount.replace(/\,/g,''));
     if (!invoice_due_date || !invoice_date || !invoice_approve_amount) {
       $("#upFrontAmount").empty().html('<b style="color:red;">Invoice Date, Invoice Due Date, Invoice Amount is required to Calculate Upfront Interest Amount</b>').show();
-      setTimeout(function(){
+      setTimeout(function(){ 
           $("#upFrontAmount").hide();
       }, 5000); // hide after 5 seconds
       return false;
